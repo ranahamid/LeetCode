bf908d8170718c00dfc9f1de8425ed6f75e78a0a
RanaHamid <RanaHamid@DESKTOP-B0DTSP4>
2022-06-17 12:58:59 +0600

    Add project files.

---
+++ LeetCode/1 Two Sum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	internal class Two_Sum
+	{
+		/// <summary>
+		/// int[] nums = { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+		// var result = TwoSum(nums, 11);
+		// Console.WriteLine($"[{result[0]},{result[1]}]");
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public static int[] TwoSum(int[] nums, int target)
+		{
+			Dictionary<int, int> resultDic = new Dictionary<int, int>();
+			var counter = 0;
+			int[] result = { -1, -1 };
+			foreach (var item in nums)
+			{
+				var targetRequired = target - item;
+				if (resultDic.ContainsKey(targetRequired))
+				{
+					var resultIndex = resultDic[targetRequired];
+					result[0] = resultIndex;
+					result[1] = counter;
+					return result;
+				}
+				else if (!resultDic.ContainsKey(item))
+				{
+					resultDic.Add(item, counter);
+				}
+				counter++;
+			}
+			return result;
+		}
+	}
+}
