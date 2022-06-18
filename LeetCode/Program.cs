c8b36cc119fde009bb4f1931d3aeafc8fb3d6c35
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-18 12:13:07 +0600

    Container_With_Most_Water

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,29 +11,10 @@ namespace LeetCode
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
-            Rotate(nums, 3);
+        { 
+            Console.WriteLine( );
             Console.ReadKey();
         }
-        public static void Rotate(int[] nums, int k)
-        {
-            //Runtime: 333 ms, faster than 7.31% of C# online submissions for Rotate Array.
-            k = k % nums.Length;
-            Array.Reverse(nums);
-            Array.Reverse(nums, 0, k);
-            Array.Reverse(nums, k, nums.Length - k); 
-        }
-        public static void reverse(int[] nums, int i, int j)
-        {
-            while (i < j)
-            {
-                (nums[i], nums[j]) = (nums[j], nums[i]);
-                i++;
-                j--;
-            }
-
-        }
+      
     }
-
 }
