b8e40182e0e146b71dd5ba1813b21cf238ff55d3
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-18 11:02:34 +0600

    d

---
+++ LeetCode/189 Rotate Array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Rotate_Array
+    {
+        public static void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            reverse(nums, 0, nums.Length - 1);// reverse all
+            reverse(nums, 0, k - 1); //reverse first to k-1
+            reverse(nums, k, nums.Length - 1); //reverse k to last
+            //Console.WriteLine(nums);
+        }
+        public static void reverse(int[] nums, int i, int j)
+        {
+            while (i < j)
+            {
+
+                //(nums[i], nums[j]) = (nums[j], nums[i]);
+                var temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+
+        }
+        public static void Rotate_2(int[] nums, int k)
+        {
+            //Runtime: 333 ms, faster than 7.31% of C# online submissions for Rotate Array.
+            k = k % nums.Length;
+            Array.Reverse(nums);
+            Array.Reverse(nums, 0, k);
+            Array.Reverse(nums, k, nums.Length - k);
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,23 +6,34 @@ using System.Threading.Tasks;
 
 namespace LeetCode
 {
-  
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
-            ListNode five = new ListNode(5);
-            ListNode four = new ListNode(4, five);
-            ListNode three = new ListNode(3, four);
-            ListNode two = new ListNode(2, three);
-            ListNode one = new ListNode(1,two); 
-             
-            Console.WriteLine("Hello World");
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            Rotate(nums, 3);
             Console.ReadKey();
         }
-        
+        public static void Rotate(int[] nums, int k)
+        {
+            //Runtime: 333 ms, faster than 7.31% of C# online submissions for Rotate Array.
+            k = k % nums.Length;
+            Array.Reverse(nums);
+            Array.Reverse(nums, 0, k);
+            Array.Reverse(nums, k, nums.Length - k); 
+        }
+        public static void reverse(int[] nums, int i, int j)
+        {
+            while (i < j)
+            {
+                (nums[i], nums[j]) = (nums[j], nums[i]);
+                i++;
+                j--;
+            }
+
+        }
     }
- 
+
 }
