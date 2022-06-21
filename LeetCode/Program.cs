bf1fc9d973c32362f3c2459b40cdbd701416ecb1
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-21 12:20:57 +0600

    c

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -37,12 +37,45 @@ namespace LeetCode
             //Console.Write(MyAtoi("4193 with words"));
 
             var res = new int[] { };
-            Console.Write(IntToRoman(3));
+            Console.Write(RomanToInt("MCMXCIV"));
             //Merge(res, 3, res2, 3);
             Console.ReadKey();
         }
+        /// <summary>
+        /// Runtime: 124 ms, faster than 26.16% of C# online submissions for Roman to Integer.
+        /// Memory Usage: 36.7 MB, less than 79.61% of C# online submissions for Roman to Integer.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static  int RomanToInt(string s)
+        {
+            var result = 0;
+            var chars=s.ToCharArray();
+            for(int i= chars.Length-1; i>=0; i--)
+            {
+                if (chars[i] == 'I')
+                    result += result >=5 ? -1 : 1;
 
-        
+                if (chars[i] == 'V')
+                    result += 5;
+
+                if (chars[i] == 'X')
+                    result += 10 * (result >=50 ? -1 : 1);
+
+                if (chars[i] == 'L')
+                    result += 50;
+
+                if (chars[i] == 'C')
+                    result += 100 * (result >=500 ? -1 : 1);
+
+                if (chars[i] == 'D')
+                    result += 500;
+
+                if (chars[i] == 'M')
+                    result += 1000;
+            }
+            return result;
+        }
 
 
 
