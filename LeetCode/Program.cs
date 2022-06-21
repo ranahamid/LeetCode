f9d9c80717372e120d83ef6a08e9c8185c4e2a4a
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-21 12:57:59 +0600

    SubsetsWithDup

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -36,48 +36,66 @@ namespace LeetCode
             //Console.Write(MyAtoi2("-91283472332"));
             //Console.Write(MyAtoi("4193 with words"));
 
-            var res = new int[] { };
-            Console.Write(RomanToInt("MCMXCIV"));
+            var res = new int[] { 0 };
+            Console.Write(SubsetsWithDup(res));
             //Merge(res, 3, res2, 3);
             Console.ReadKey();
         }
-        /// <summary>
-        /// Runtime: 124 ms, faster than 26.16% of C# online submissions for Roman to Integer.
-        /// Memory Usage: 36.7 MB, less than 79.61% of C# online submissions for Roman to Integer.
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        public static  int RomanToInt(string s)
+
+        public static  IList<IList<int>> SubsetsWithDup(int[] nums)
         {
-            var result = 0;
-            var chars=s.ToCharArray();
-            for(int i= chars.Length-1; i>=0; i--)
+            int[] output = new int[0];
+            List<IList<int>> list = new List<IList<int>>();
+            list.Add(new List<int>()); 
+            Dictionary<int, string > dictionary = new Dictionary<int, string >();
+            var counter = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (chars[i] == 'I')
-                    result += result >=5 ? -1 : 1;
+                if (i == 0)
+                {
+                    list.Add(new List<int> { nums[i] });
+                    dictionary.Add(counter++, ConvertToStringFromIntCommas(new List<int> { nums[i] }));
+                }
+                else
+                {
+                    var existingItems = list.Select(x => x.ToList()).ToList();
+                    foreach (var item in existingItems)
+                    {
+                        item.Add(nums[i]);
+                    }
+                    foreach (var item in existingItems)
+                    {
+                        var st = ConvertToStringFromIntCommas(item);
+                        if (dictionary.ContainsValue(st))
+                        {
 
-                if (chars[i] == 'V')
-                    result += 5;
-
-                if (chars[i] == 'X')
-                    result += 10 * (result >=50 ? -1 : 1);
-
-                if (chars[i] == 'L')
-                    result += 50;
-
-                if (chars[i] == 'C')
-                    result += 100 * (result >=500 ? -1 : 1);
-
-                if (chars[i] == 'D')
-                    result += 500;
-
-                if (chars[i] == 'M')
-                    result += 1000;
+                        }
+                        else
+                        {
+                            list.Add(item);
+                            dictionary.Add(counter++, st);
+                        }
+                        
+                    }
+                      
+                }
             }
+             
+            Console.WriteLine(list.Count());
+            var result = list.Select(x => (IList<int>)x).ToList();
             return result;
         }
 
+        public static  string ConvertToStringFromIntCommas(List<int> names)
+        {
+            if (names != null && names.Count > 0)
+            {
+                var result = string.Join(",", names.ToArray());
+                return result;
+            }
 
+            return "";
+        }
 
     }
 
