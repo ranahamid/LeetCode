279dfc13fd3459b55d014f570a3ec42df2dae8ed
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-22 13:03:28 +0600

    58

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -49,41 +49,6 @@ namespace LeetCode
         
 
     }
-    /// <summary>
-    /// Runtime: 242 ms, faster than 46.67% of C# online submissions for Design Parking System.
-    /// Memory Usage: 46.7 MB, less than 39.09% of C# online submissions for Design Parking System.
-    /// </summary>
-    public class ParkingSystem
-    {
-        private int bigCar=0;
-        private int mediumCar = 0;
-        private int smallCar = 0;
-        public ParkingSystem(int big, int medium, int small)
-        {
-            bigCar=big;
-            mediumCar=medium;
-            smallCar=small;
-        }
-
-        public bool AddCar(int carType)
-        {
-            if (carType == 1 && bigCar >0)
-            {
-                bigCar--;
-                return true;
-            }
-            if (carType == 2 && mediumCar > 0)
-            {
-                mediumCar--;
-                return true;
-            }
-            if (carType == 3 && smallCar >0)
-            {
-                smallCar--;
-                return true;
-            }
-            return false;
-        }
-    }
+   
 
 }
