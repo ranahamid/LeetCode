fa5a03ca65eb7eda9520e404d9e9b1293e4948ab
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-24 10:30:48 +0600

    Maximum69Number

---
+++ LeetCode/1323 Maximum 69 Number.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1323_Maximum_69_Number
+    {
+
+        /// <summary>
+        /// Runtime: 48 ms, faster than 7.14% of C# online submissions for Maximum 69 Number.
+        /// Memory Usage: 25.6 MB, less than 25.00% of C# online submissions for Maximum 69 Number.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+
+        public static int Maximum69Number(int num)
+        {
+            var digits = GetDigits(num);
+            int result = 0;
+            var isChanged = false;
+            for (int i = digits.Count - 1; i >= 0; i--)
+            {
+                if (digits[i] == 6 && !isChanged)
+                {
+                    digits[i] = 9;
+                    isChanged = true;
+                }
+                result = result * 10 + digits[i];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Runtime: 40 ms, faster than 19.05% of C# online submissions for Maximum 69 Number.
+        /// Memory Usage: 25.7 MB, less than 25.00% of C# online submissions for Maximum 69 Number.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+
+        public static int Maximum69Number_2(int num)
+        {
+            var digits = GetDigits(num);
+            int result = 0;
+            for (int i = digits.Count - 1; i >= 0; i--)
+            {
+                if (digits[i] == 6)
+                {
+                    digits[i] = 9;
+                    break;
+                }
+            }
+            for (int i = digits.Count - 1; i >= 0; i--)
+            {
+                result = result * 10 + digits[i];
+            }
+            return result;
+        }
+        public static List<int> GetDigits(int num)
+        {
+            List<int> digitList = new List<int>();
+            while (num != 0)
+            {
+                var val = num % 10;
+                digitList.Add(val);
+                num = num / 10;
+            }
+            return digitList;
+        }
+
+    }
+}
