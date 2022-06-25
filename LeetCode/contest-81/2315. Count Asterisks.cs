cd4b904448276ec4741d7ee86bd98f7d03bbb9ee
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-25 22:18:03 +0600

    MaximumXOR

---
+++ LeetCode/contest-81/2315. Count Asterisks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.contest_81
+{
+    internal class _6104_Count_Asterisks
+    {
+        /// <summary>
+        /// Runtime: 115 ms, faster than 100.00% of C# online submissions for Count Asterisks.
+        /// Memory Usage: 34.8 MB, less than 100.00% of C# online submissions for Count Asterisks.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int CountAsterisks(string s)
+        {
+            var chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '|')
+                {
+                    i++;
+                    while (chars[i] != '|')
+                    {
+                        chars[i] = '-';
+                        i++;
+                    }
+                }
+            }
+            var count = chars.Where(x => x == '*').Count();
+            return count;
+        }
+    }
+}
---
+++ LeetCode/contest-81/2317 Maximum XOR After Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.contest_81
+{
+    internal class _2317_Maximum_XOR_After_Operations
+    {
+
+        /// <summary>
+        /// Runtime: 332 ms, faster than 100.00% of C# online submissions for Maximum XOR After Operations .
+        /// Memory Usage: 47.5 MB, less than 100.00% of C# online submissions for Maximum XOR After Operations .
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MaximumXOR(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result = result | nums[i];
+            }
+            return result;
+        } 
+
+    }
+}
