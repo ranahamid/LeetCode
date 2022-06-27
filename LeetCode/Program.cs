7fd18568bbfc178441dea3b7cfaedd51dc09c76e
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-27 21:48:23 +0600

    s

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -77,47 +77,7 @@ namespace LeetCode
             Console.ReadKey();
         }
 
-
-        public static int CalPoints(string[] ops)
-        {
-            
-            var result = new int[ops.Length];
-            var counter = 0;
-            for(int i = 0;i < ops.Length; i++)
-            {
-                if(ops[i]=="C")
-                {
-                    result[counter-1] = 0;
-                    counter--;
-                }
-                else if (ops[i] == "D")
-                {
-                    var number = result[counter - 1] *2;
-                    result[counter++] = number;
-                }
-                else if (ops[i] == "+")
-                {
-                    var number = result[counter - 1] + result[counter - 2];
-                    result[counter++] = number;
-                }
-                else
-                {
-                    if(Int32.TryParse(ops[i], out int number))
-                    {
-                        result[counter++] = number;
-                    }                   
-
-                }
-            }
-            var sum = 0;
-            for(int i=0;i<ops.Length;i++)
-            {
-                sum=sum+ result[i];
-            }
-            return sum;
-        }
-
-
+      
 
 
 
