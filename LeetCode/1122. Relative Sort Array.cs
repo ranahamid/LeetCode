38f540b03f08b1d5b1cd49786dfbdad297588b7b
Rana Hamid <rhamid@mymedicalhub.com>
2022-06-30 09:46:44 +0600

    _766__Toeplitz_Matrix

---
+++ LeetCode/1122. Relative Sort Array.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class _1122__Relative_Sort_Array {
+        
+        /// <summary>
+        /// Runtime: 178 ms, faster than 61.64% of C# online submissions for Relative Sort Array.
+        /// Memory Usage: 41.6 MB, less than 30.82% of C# online submissions for Relative Sort Array.
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <returns></returns>
+        public static int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            var result = new int[arr1.Length];
+            var intersect = arr1.Where(x=>!arr2.Contains(x)).ToList();
+            var counter = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                result[counter++] = arr2[i];
+                var values = arr1.Where(x => x == arr2[i]).ToList();
+                for (int j = 0; j < values.Count-1; j++)
+                {
+                    result[counter++] = values[j];
+                }
+            }
+            intersect.Sort();
+            foreach (var item in intersect)
+            {
+                result[counter++] = item;
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ LeetCode/766. Toeplitz Matrix.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+    public class _766__Toeplitz_Matrix {
+        
+        /// <summary>
+        /// Runtime: 139 ms, faster than 59.40% of C# online submissions for Toeplitz Matrix.
+        /// Memory Usage: 41.4 MB, less than 51.13% of C# online submissions for Toeplitz Matrix.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            int row = matrix.Length;
+            int column = matrix[0].Length;
+            for (int i = 0; i < row-1; i++)
+            {
+                for (int j = 0; j < column-1; j++)
+                {
+                    if (matrix[i][j] != matrix[i + 1][j + 1])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
