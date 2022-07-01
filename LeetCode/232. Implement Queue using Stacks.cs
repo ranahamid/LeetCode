246103f40182ae29722cf1ea6749005db220a319
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-01 12:53:57 +0600

    LeetCode

---
+++ LeetCode/232. Implement Queue using Stacks.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// Runtime: 190 ms, faster than 17.42% of C# online submissions for Implement Queue using Stacks.
+    /// Memory Usage: 39.1 MB, less than 59.43% of C# online submissions for Implement Queue using Stacks.
+    /// </summary>
+    public class MyQueue
+    {
+        Stack<int> InStack;
+        Stack<int> OutStack; 
+        public MyQueue()
+        {
+            InStack = new Stack<int>();
+            OutStack = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            InStack.Push(x);
+        }
+        public void FillOutStack()
+        {
+            if (InStack.Count != 0)
+            {
+                while (InStack.Count > 0)
+                {
+                    OutStack.Push(InStack.Pop());
+                }
+            }
+        }
+       
+        public int Pop()
+        {
+            if (OutStack.Count != 0)
+            {
+                return OutStack.Pop();
+            }
+            FillOutStack();            
+            return OutStack.Pop();
+           
+        }
+
+        public int Peek()
+        {
+            if (OutStack.Count != 0)
+            {
+                return OutStack.Peek();
+            }
+            FillOutStack();            
+            return OutStack.Peek();
+          
+        }
+
+        public bool Empty()
+        {
+            if(InStack.Count == 0 && OutStack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Peek();
+     * bool param_4 = obj.Empty();
+     */
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -64,19 +64,18 @@ namespace LeetCode
             {
                 "10","6","9","3","+","-11","*","/","*","17","+","5","+"
             };
-            var res1 = new int[] { 1, 3 };
-            var res2 = new int[] { 2, 4 };
+            var res1 = new int[] { 1, 4, 2, 3 };
+            var res2 = new int[] { -4, -3, 6, 10, 20, 30 };
             var res3 = new int[] { 3, 1, 5, 4, 2 };
             // var res3 = new int[] {3,8,-10,23,19,-4,-14,27};
             // Console.WriteLine(HeightChecker(res1));
-            Console.WriteLine(EvalRPN(resW2));
+            // Console.WriteLine(FindTheDistanceValue(res1, res2,3));
             Console.ReadKey();
         }
 
 
-      
 
-
+       
 
 
 
