e9aedcc836e3e70b379001143ed984f0b333fa0e
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-05 14:26:23 +0600

    d

--- LeetCode/Codeforces/Contest 804/A. The Third Three Number Problem.cs
+++ LeetCode/Codeforces/Contest 804/A. The Third Three Number Problem.cs
@@ -1,43 +1,30 @@
-﻿//using System;
-
-
-//namespace LeetCode
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int intTemp = Convert.ToInt32(Console.ReadLine());
-//            for (int index = 0; index < intTemp; index++)
-//            {
-//                var n = Convert.ToInt32(Console.ReadLine());
-//                bool isFound = false;
-//                for (int i = 0; i < n; i++)
-//                {
-//                    for (int j = 0; j < n; j++)
-//                    {
-//                        for (int k = n; k >= 0; k--)
-//                        {
-//                            if ((i ^ j) + (j ^ k) + (j ^ k) == n)
-//                            {
-//                                Console.WriteLine($"{i} {j} {k}");
-//                                isFound = true;
-//                            }
-//                            if (isFound)
-//                                break;
-//                        }
-//                        if (isFound)
-//                            break;
-//                    }
-//                    if (isFound)
-//                        break;
-//                }
-//                if (!isFound)
-//                    Console.WriteLine(-1);
-//            }
-//        } 
-//    }
-//}
-
-
-//TLE
+﻿using System;
+namespace LeetCode
+{
+    internal class Program
+    {
+        /// <summary>
+        /// https://codeforces.com/contest/1699/problem/A
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int intTemp = Convert.ToInt32(Console.ReadLine());
+            for (int index = 0; index < intTemp; index++)
+            {
+                var n = Convert.ToInt32(Console.ReadLine()); 
+                if (n % 2 == 1)
+                {
+                    Console.WriteLine(-1); 
+                }
+                else
+                {
+                    int i=0, j=0;
+                    int k = n >> 1;
+                    Console.WriteLine($"{i} {j} {k}");
+                } 
+            }
+        }
+    }
+}
+ 
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,90 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
+//using System.Numerics;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.XPath;
 
-namespace LeetCode
-{
+//namespace LeetCode
+//{
 
     
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[][] nums = new int[4][]
-            {
-                new int[2] { 5,10 },
-                new int[2] { 2,5 },
-                new int[2] { 4,7 },
-                new int[2] { 3,9 },
-            };
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[][] nums = new int[4][]
+//            {
+//                new int[2] { 5,10 },
+//                new int[2] { 2,5 },
+//                new int[2] { 4,7 },
+//                new int[2] { 3,9 },
+//            };
 
-            int[][] nums1 = new int[2][]
-            {
-                new int[2] { 0, 1 },
-                new int[2] { 1, 1 },
-            };
+//            int[][] nums1 = new int[2][]
+//            {
+//                new int[2] { 0, 1 },
+//                new int[2] { 1, 1 },
+//            };
 
 
-            #region node
+//            #region node
 
-            //ListNode eig = new ListNode(3);
-            //ListNode sev = new ListNode(3, eig);
-            //ListNode six = new ListNode(2, sev);
-            //ListNode five = new ListNode(1, six);
-            //ListNode list2 = new ListNode(1, five);
+//            //ListNode eig = new ListNode(3);
+//            //ListNode sev = new ListNode(3, eig);
+//            //ListNode six = new ListNode(2, sev);
+//            //ListNode five = new ListNode(1, six);
+//            //ListNode list2 = new ListNode(1, five);
 
-            //ListNode fifteen = new ListNode(0);
-            //ListNode fourtenn = new ListNode(0, fifteen);
+//            //ListNode fifteen = new ListNode(0);
+//            //ListNode fourtenn = new ListNode(0, fifteen);
          
-            //ListNode thirteen = new ListNode(0);
-            //ListNode twelve = new ListNode(5, thirteen);
-            //ListNode eleven = new ListNode(5, twelve);
-            //ListNode ten = new ListNode(2, eleven);
-            //ListNode nine = new ListNode(4, ten);
-            //ListNode eight = new ListNode(9, nine);
-            //ListNode seven = new ListNode(7, eight);
-            //ListNode six = new ListNode(1, seven);
-            //ListNode five = new ListNode(8, six);
-            //ListNode four = new ListNode(6, five);
-            //ListNode three = new ListNode(2, four);
-            //ListNode two = new ListNode(0, three);
-            //ListNode list1 = new ListNode(3, two);
+//            //ListNode thirteen = new ListNode(0);
+//            //ListNode twelve = new ListNode(5, thirteen);
+//            //ListNode eleven = new ListNode(5, twelve);
+//            //ListNode ten = new ListNode(2, eleven);
+//            //ListNode nine = new ListNode(4, ten);
+//            //ListNode eight = new ListNode(9, nine);
+//            //ListNode seven = new ListNode(7, eight);
+//            //ListNode six = new ListNode(1, seven);
+//            //ListNode five = new ListNode(8, six);
+//            //ListNode four = new ListNode(6, five);
+//            //ListNode three = new ListNode(2, four);
+//            //ListNode two = new ListNode(0, three);
+//            //ListNode list1 = new ListNode(3, two);
 
-            #endregion
+//            #endregion
 
-            var paths = new List<IList<string>>()
-            {
-                new List<string>() { "London", "New York" },
-                new List<string>() { "New York", "Lima" },
-                new List<string>() { "Lima", "Sao Paulo" },
-            };
-            var resW1 = new string[] { "a", "ab" };
-            var resW2 = new string[]
-            {
-                "10","6","9","3","+","-11","*","/","*","17","+","5","+"
-            };
-            var res1 = new int[] { 1, 7, 4, 9, 2, 5 };
-            var res2 = new int[] { -4, -3, 6, 10, 20, 30 };
-            var res3 = new int[] { 3, 1, 5, 4, 2 };
-            // var res3 = new int[] {3,8,-10,23,19,-4,-14,27};
-            // Console.WriteLine(HeightChecker(res1));
-            //Console.WriteLine(ReformatDate("20th Oct 2052"));
-
-
+//            var paths = new List<IList<string>>()
+//            {
+//                new List<string>() { "London", "New York" },
+//                new List<string>() { "New York", "Lima" },
+//                new List<string>() { "Lima", "Sao Paulo" },
+//            };
+//            var resW1 = new string[] { "a", "ab" };
+//            var resW2 = new string[]
+//            {
+//                "10","6","9","3","+","-11","*","/","*","17","+","5","+"
+//            };
+//            var res1 = new int[] { 1, 7, 4, 9, 2, 5 };
+//            var res2 = new int[] { -4, -3, 6, 10, 20, 30 };
+//            var res3 = new int[] { 3, 1, 5, 4, 2 };
+//            // var res3 = new int[] {3,8,-10,23,19,-4,-14,27};
+//            // Console.WriteLine(HeightChecker(res1));
+//            //Console.WriteLine(ReformatDate("20th Oct 2052"));
 
 
 
 
 
-            Console.ReadKey();
-        }
+
+
+//            Console.ReadKey();
+//        }
 
 
 
-    }
-}
+//    }
+//}
