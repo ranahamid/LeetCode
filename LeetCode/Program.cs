7ed662c298513caf071deba42ac2a37d502bbc94
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-08 14:53:46 +0600

    s

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -77,20 +77,7 @@ namespace LeetCode
             Console.ReadKey();
         }
 
-        public static int GetXORSum(int[] arr1, int[] arr2)
-        {
-            int xor1=0,xor2=0;
-            for (int i=0; i < arr1.Length; i++){
-                xor1=xor1 ^ arr1[i];
-            }
-            for (int i = 0; i < arr2.Length; i++)
-            {
-                xor2 = xor2 ^ arr2[i];
-            }
-            int result = xor1 & xor2;
-            return result;
-        }
-
+      
 
 
 
