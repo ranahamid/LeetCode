c42e4786261ef589e4aefacaae8179f254c06042
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-08 19:45:21 +0600

    v

---
+++ LeetCode/SQL/1667. Fix Names in a Table.sql
@@ -0,0 +1,7 @@
+﻿/* Write your T-SQL query statement below */
+select user_id, upper( substring( name,1,1))+  lower(substring( name,2,len(name)) ) as name
+from users
+order by user_id 
+
+--Runtime: 2242 ms, faster than 35.78% of MS SQL Server online submissions for Fix Names in a Table.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Fix Names in a Table.
