e1b22b6e148ff3ea8b74f625d2031c557cb374f2
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-09 22:31:55 +0600

    cd

---
+++ LeetCode/Go/70. Climbing Stairs.go
@@ -0,0 +1,18 @@
+﻿func climbStairs(n int) int {
+       if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 2
+	}
+	//var result = [10]int{}
+	var result = make([]int, n+1)
+	result[1] = 1
+	result[2] = 2
+	for i := 3; i <= n; i++ {
+		result[i] = result[i-2] + result[i-1]
+	}
+	return result[n]
+}
+
+//Runtime: 0 ms, faster than 100.00% of Go online submissions for Climbing Stairs.
+//Memory Usage: 1.9 MB, less than 41.30% of Go online submissions for Climbing Stairs.
