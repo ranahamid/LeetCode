d8841217cef9fe9b1b17495896547ebc8366a27b
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-11 09:07:56 +0600

    PlusOne

---
+++ LeetCode/66. Plus One.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _66
+    {
+        /// <summary>
+        /// Runtime: 218 ms, faster than 34.41% of C# online submissions for Plus One.
+        /// Memory Usage: 40.8 MB, less than 81.77% of C# online submissions for Plus One.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public static int[] PlusOne(int[] digits)
+        {
+            List<int> result = new List<int>(); int carry = 0;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+
+                var nmbr = digits[i];
+                if (i == digits.Length - 1)
+                {
+                    nmbr = nmbr + 1;
+                    if (nmbr == 10)
+                    {
+                        carry = 1;
+                        nmbr = 0;
+                    }
+                    else
+                    {
+                        carry = 0;
+                    }
+                }
+                else
+                {
+                    nmbr = nmbr + carry;
+                    if (nmbr == 10)
+                    {
+                        carry = 1;
+                        nmbr = 0;
+                    }
+                    else
+                    {
+                        carry = 0;
+                    }
+                }
+
+                result.Add(nmbr);
+            }
+            if (carry == 1)
+            {
+                result.Add(1);
+            }
+            result.Reverse();
+            return result.ToArray();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -69,20 +69,20 @@ namespace LeetCode
              "xxxz","ax","bx","cx"
             };
             var res1 = new int?[] { 1, 2, 3, 4 };
-            var res2 = new int[] { 5, 4, 4 };
+            var res2 = new int[] { 1,2,9 };
             var res3 = new int[] { 6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5, 10, 8, 6, 6, 1, 0, 6, 10, 8, 2, 3, 4 };
             // var res3 = new int[] {3,8,-10,23,19,-4,-14,27};
             //Solution s = new Solution();
             //var rd=s.LevelOrderPrint(res1);
             //Console.WriteLine(IsSameTree(rd,rd));
 
-            Console.WriteLine(IsValid("([)]"));
+            Console.WriteLine(PlusOne(res2));
             Console.ReadKey();
         }
 
 
-     
-
+   
+      
 
 
 
