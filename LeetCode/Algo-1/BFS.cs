65a1137ec8145725f85b8e42056d3f83b79a5c6e
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-11 07:32:18 +0600

    _199

--- LeetCode/Algo-1/BFS.cs
+++ LeetCode/Algo-1/BFS.cs
@@ -31,6 +31,35 @@ namespace LeetCode
             return result.ToArray();
         }
 
-     
+        public static IList<int> RightSideView(TreeNode root)
+        {
+            var result = new List<TreeNode>();
+            if (root == null)
+                return new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var size = queue.Count();
+                for(int i = 0; i < size; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    if(i==0)
+                    {
+                        result.Add(node);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                }
+                
+            }
+            return result.Select(x=>x.val).ToArray();
+        }
     }
 }
