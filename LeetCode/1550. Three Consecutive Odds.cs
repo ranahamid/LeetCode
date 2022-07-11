276c43db3c5cf8836f947b06cf7458ab70679aac
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-11 15:55:08 +0600

    _1550

--- LeetCode/1550. Three Consecutive Odds.cs
+++ LeetCode/1550. Three Consecutive Odds.cs
@@ -9,15 +9,14 @@ namespace LeetCode
     internal class _1550
     {
         /// <summary>
-        /// Runtime: 167 ms, faster than 12.00% of C# online submissions for Three Consecutive Odds.
-        /// Memory Usage: 39.5 MB, less than 52.00% of C# online submissions for Three Consecutive Odds.
+        /// Runtime: 125 ms, faster than 62.00% of C# online submissions for Three Consecutive Odds.
+        /// Memory Usage: 39.2 MB, less than 86.00% of C# online submissions for Three Consecutive Odds.
         /// </summary>
         /// <param name="arr"></param>
         /// <returns></returns>
         public static bool ThreeConsecutiveOdds(int[] arr)
         {
-            if (arr == null || arr.Length < 3)
-                return false;
+            
             for (int i = 0; i < arr.Length - 2; i++)
             {
                 if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1)
