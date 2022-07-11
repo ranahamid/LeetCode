6ac1340e90e926b6d39bd3d67da702d1fb6f6936
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-11 15:47:30 +0600

    CanMakeArithmeticProgression

---
+++ LeetCode/1502. Can Make Arithmetic Progression From Sequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1502
+    {
+        public static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            if (arr.Length == 0 || arr.Length == 1)
+            {
+                return false;
+            }
+            var diff = arr[0] - arr[1];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i - 1] - arr[i] == diff)
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
