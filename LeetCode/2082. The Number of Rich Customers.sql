7a807dfe5fc0413ebc3253434a3ff75863d859e8
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-13 17:50:55 +0600

    ss

---
+++ LeetCode/2082. The Number of Rich Customers.sql
@@ -0,0 +1,13 @@
+﻿/* Write your T-SQL query statement below */
+select count(distinct customer_id ) as rich_count 
+from Store
+where amount>500
+
+
+--Runtime: 656 ms, faster than 100.00% of MS SQL Server online submissions for The Number of Rich Customers.
+-- Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for The Number of Rich Customers.
+
+
+--my sql
+--Runtime: 350 ms, faster than 93.42% of MySQL online submissions for The Number of Rich Customers.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for The Number of Rich Customers.
