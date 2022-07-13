868c8424c18ab22344bd1cdbaea7c3a359a27dca
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-13 16:53:01 +0600

    SQL Server

---
+++ LeetCode/Premium/1251. Average Selling Price.sql
@@ -0,0 +1,7 @@
+﻿ MYSQL
+  select p.product_id , ROUND( sum(units *price)/sum(units),2) as  average_price
+  from Prices as p, UnitsSold  as u
+ 
+  where  p.product_id = u.product_id AND purchase_date between start_date and end_date   
+  group by p.product_id
+
---
+++ LeetCode/Premium/1421. NPV Queries.sql
@@ -0,0 +1,11 @@
+﻿ select q.id   , q.year , (case when  n.npv is null then 0 else  n.npv end) as npv
+from Queries  as q
+left join NPV as n on q .id= n .id and  q .year   = n .year     
+--my sql
+--Runtime: 1005 ms, faster than 52.32% of MySQL online submissions for NPV Queries.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for NPV Queries.
+
+
+-- SQL Server
+--Runtime: 1826 ms, faster than 48.60% of MS SQL Server online submissions for NPV Queries.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for NPV Queries.
