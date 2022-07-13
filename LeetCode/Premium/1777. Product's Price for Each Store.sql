52cb8358d3736a60a6c1b621f7b6612433d90da6
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-13 14:46:10 +0600

    product_id

---
+++ LeetCode/Premium/1777. Product's Price for Each Store.sql
@@ -0,0 +1,6 @@
+﻿select product_id ,
+sum(case when store='store1' then price end) as store1,
+sum(case when store='store2' then price end) as store2,
+sum(case when store='store3' then price end) as store3
+from Products 
+group by product_id
