d638f54b436a15dc9268cd2e6f6d5fa704f48aea
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-15 22:01:10 +0600

    d

---
+++ LeetCode/Codeforces/1705/C. Mark and His Unfinished Essay.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        /// <summary>
+        /// https://codeforces.com/contest/1705/problem/C
+        /// MEMORY LIMIT Exceed
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int intTemp = Convert.ToInt32(Console.ReadLine());
+            var ind = 0;
+            string test = "";
+            int a = 0, b = 0, n, c, r;
+            StringBuilder sb = new StringBuilder();
+            for (int index = 0; index < intTemp; index++)
+            {
+                //Read line, and split it by whitespace into an array of strings
+                string[] tokens = Console.ReadLine().Split();
+                //Parse element 0
+                n = int.Parse(tokens[0]);
+                c = int.Parse(tokens[1]);
+                r = int.Parse(tokens[1]);
+
+                string s = Console.ReadLine();
+                sb = new StringBuilder(s);
+                s = string.Empty;
+
+                for (int i = 0; i < c; i++)
+                {
+                    string[] numbers = Console.ReadLine().Split();
+                    a = int.Parse(numbers[0]);
+                    b = int.Parse(numbers[1]);
+
+                    test = sb.ToString(a - 1, b - a + 1);
+                    sb.Append(test);
+                }
+
+                for (int i = 0; i < r; i++)
+                {
+                    ind = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(sb[ind - 1]);
+                }
+                sb.Clear();
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,141 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
+//using System.Numerics;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.XPath;
 
-namespace LeetCode
-{
-
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[][] nums = new int[3][]
-            {
-                new int[2] { 3,50 },
-                new int[2] { 7,10 },
-                new int[2] { 12,25 },
-            };
-
-            int[][] nums1 = new int[2][]
-            {
-                new int[2] { 0, 1 },
-                new int[2] { 1, 1 },
-            };
+//namespace LeetCode
+//{
 
 
-            #region node
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[][] nums = new int[3][]
+//            {
+//                new int[2] { 3,50 },
+//                new int[2] { 7,10 },
+//                new int[2] { 12,25 },
+//            };
 
-            //ListNode eig = new ListNode(3);
-            //TreeNode sev = new TreeNode(0);
-            //TreeNode six = new TreeNode(3);
-            //TreeNode five = new TreeNode();
-            //TreeNode list2 = new TreeNode(2, five, six);
-
-            //ListNode fifteen = new ListNode(0);
-            //ListNode fourtenn = new ListNode(0, fifteen);
-
-            //ListNode thirteen = new ListNode(0);
-            //ListNode twelve = new ListNode(5, thirteen);
-            //ListNode eleven = new ListNode(5, twelve);
-            //ListNode ten = new ListNode(2, eleven);
-            //ListNode nine = new ListNode(4, ten);
-            //ListNode eight = new ListNode(9, nine);
-            //ListNode seven = new ListNode(7, eight);
-            //ListNode six = new ListNode(1, seven);
-            //ListNode five = new ListNode(8, six);
-            //ListNode four = new ListNode(6, five);
-            //ListNode three = new ListNode(2, four);
-            //ListNode two = new ListNode(0, three);
-            //ListNode list1 = new ListNode(3, two);
-
-            #endregion
-
-            var paths = new List<IList<string>>()
-            {
-                new List<string>() { "London", "New York" },
-                new List<string>() { "New York", "Lima" },
-                new List<string>() { "Lima", "Sao Paulo" },
-            };
-            var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
-            var resW2 = new string[]
-            {
-                "ab","a"
-            };
-            var res1 = new int[] { 2, 3, 2 };
-            var res2 = new int[] { 1, 2, 9 };
-            var res3 = new int[] { 2, 4, 9, 3 };
-            Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
-
-            Console.ReadKey();
-        }
-        //public class TreeNode
-        //{
-        //    public int val;
-        //    public TreeNode left;
-        //    public TreeNode right;
-        //    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        //    {
-        //        this.val = val;
-        //        this.left = left;
-        //        this.right = right;
-        //    }
-        //}
+//            int[][] nums1 = new int[2][]
+//            {
+//                new int[2] { 0, 1 },
+//                new int[2] { 1, 1 },
+//            };
 
 
+//            #region node
 
-        public static bool WordPattern(string pattern, string s)
-        {
-            var dicFirst=new Dictionary<char, string>();
-            var dicSec = new Dictionary<string, char>();
+//            //ListNode eig = new ListNode(3);
+//            //TreeNode sev = new TreeNode(0);
+//            //TreeNode six = new TreeNode(3);
+//            //TreeNode five = new TreeNode();
+//            //TreeNode list2 = new TreeNode(2, five, six);
 
-            var words = s.Split(' ');
-            if (words.Length != pattern.Length)
-                return false;
+//            //ListNode fifteen = new ListNode(0);
+//            //ListNode fourtenn = new ListNode(0, fifteen);
+
+//            //ListNode thirteen = new ListNode(0);
+//            //ListNode twelve = new ListNode(5, thirteen);
+//            //ListNode eleven = new ListNode(5, twelve);
+//            //ListNode ten = new ListNode(2, eleven);
+//            //ListNode nine = new ListNode(4, ten);
+//            //ListNode eight = new ListNode(9, nine);
+//            //ListNode seven = new ListNode(7, eight);
+//            //ListNode six = new ListNode(1, seven);
+//            //ListNode five = new ListNode(8, six);
+//            //ListNode four = new ListNode(6, five);
+//            //ListNode three = new ListNode(2, four);
+//            //ListNode two = new ListNode(0, three);
+//            //ListNode list1 = new ListNode(3, two);
+
+//            #endregion
+
+//            var paths = new List<IList<string>>()
+//            {
+//                new List<string>() { "London", "New York" },
+//                new List<string>() { "New York", "Lima" },
+//                new List<string>() { "Lima", "Sao Paulo" },
+//            };
+//            var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
+//            var resW2 = new string[]
+//            {
+//                "ab","a"
+//            };
+//            var res1 = new int[] { 2, 3, 2 };
+//            var res2 = new int[] { 1, 2, 9 };
+//            var res3 = new int[] { 2, 4, 9, 3 };
+//            Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
+
+//            Console.ReadKey();
+//        }
+//        //public class TreeNode
+//        //{
+//        //    public int val;
+//        //    public TreeNode left;
+//        //    public TreeNode right;
+//        //    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+//        //    {
+//        //        this.val = val;
+//        //        this.left = left;
+//        //        this.right = right;
+//        //    }
+//        //}
+
+//        /// <summary>
+//        /// https://leetcode.com/problems/word-pattern/
+//        /// </summary>
+//        /// <param name="pattern"></param>
+//        /// <param name="s"></param>
+//        /// <returns></returns>
+
+//        public static bool WordPattern(string pattern, string s)
+//        {
+//            var dicFirst=new Dictionary<char, string>();
+//            var dicSec = new Dictionary<string, char>();
+
+//            var words = s.Split(' ');
+//            if (words.Length != pattern.Length)
+//                return false;
             
-            for (int i = 0; i <pattern.Length;i++)
-            {
-                var currentVal = words[i];
-                if (dicFirst.ContainsKey(pattern[i]))
-                {
-                    var dicVal=dicFirst[pattern[i]];                   
-                    if (!currentVal.Equals(dicVal))
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    dicFirst[pattern[i]] = currentVal;
-                }
-            }
-            for (int i = 0; i < words.Length; i++)
-            {
-                var currentVal = pattern[i];
-                if (dicFirst.ContainsKey(pattern[i]))
-                {
-                    var dicVal = dicFirst[pattern[i]];
-                    if (!currentVal.Equals(dicVal))
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    dicFirst[pattern[i]] = currentVal;
-                }
-            }
+//            for (int i = 0; i <pattern.Length;i++)
+//            {
+//                var currentVal = words[i];
+//                if (dicFirst.ContainsKey(pattern[i]))
+//                {
+//                    var dicVal=dicFirst[pattern[i]];                   
+//                    if (!currentVal.Equals(dicVal))
+//                    {
+//                        return false;
+//                    }
+//                }
+//                else
+//                {
+//                    dicFirst[pattern[i]] = currentVal;
+//                }
+//            }
+//            for (int i = 0; i < words.Length; i++)
+//            {
+//                var currentVal = pattern[i];
+//                if (dicFirst.ContainsKey(pattern[i]))
+//                {
+//                    var dicVal = dicFirst[pattern[i]];
+//                    if (!currentVal.Equals(dicVal))
+//                    {
+//                        return false;
+//                    }
+//                }
+//                else
+//                {
+//                    dicFirst[pattern[i]] = currentVal;
+//                }
+//            }
 
-            return true;
+//            return true;
 
-        }
-
-
+//        }
 
 
 
@@ -144,7 +147,9 @@ namespace LeetCode
 
 
 
-    }
 
 
-}
+//    }
+
+
+//}
