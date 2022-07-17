e243cc2f3c3e66c58e4bf71a4a99e45b82224bb4
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-17 19:24:09 +0600

    s

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,89 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
-
-namespace LeetCode
-{
-
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[][] nums = new int[][]
-            {
+﻿int[][] nums = new int[][]
+          {
                 new int[2] {9,4}, new int[2]{6,1}, new int[2]{3,8}, new int[2]{12,9}, new int[2]{11,4}, new int[2]{4,9}, new int[2]{2,7},
                  new int[2]{10,3}, new int[2]{13,1}, new int[2]{13,1}, new int[2]{6,1}, new int[2]{5,10}
-            };
+          };
 
-            int[][] nums1 = new int[2][]
-            {
+int[][] nums1 = new int[2][]
+{
                 new int[2] { 0, 1 },
                 new int[2] { 1, 1 },
-            };
+};
 
 
-            #region node
+#region node
 
-            //ListNode eig = new ListNode(3);
-            //TreeNode sev = new TreeNode(4);
-            //TreeNode six = new TreeNode(3);
-            //TreeNode five = new TreeNode(2,null,sev);
-            //TreeNode list2 = new TreeNode(1, five, six);
+//ListNode eig = new ListNode(3);
+//TreeNode sev = new TreeNode(4);
+//TreeNode six = new TreeNode(3);
+//TreeNode five = new TreeNode(2,null,sev);
+//TreeNode list2 = new TreeNode(1, five, six);
 
-            //ListNode fifteen = new ListNode(0);
-            //ListNode fourtenn = new ListNode(0, fifteen);
+//ListNode fifteen = new ListNode(0);
+//ListNode fourtenn = new ListNode(0, fifteen);
 
-            //ListNode thirteen = new ListNode(0);
-            //ListNode twelve = new ListNode(5, thirteen);
-            //ListNode eleven = new ListNode(5, twelve);
-            //ListNode ten = new ListNode(2, eleven);
-            //ListNode nine = new ListNode(4, ten);
-            //ListNode eight = new ListNode(9, nine);
-            //ListNode seven = new ListNode(7, eight);
-            //ListNode six = new ListNode(1, seven);
-            //ListNode five = new ListNode(8, six);
-            //ListNode four = new ListNode(6, five);
-            //ListNode three = new ListNode(2, four);
-            //ListNode two = new ListNode(0, three);
-            //ListNode list1 = new ListNode(3, two);
+//ListNode thirteen = new ListNode(0);
+//ListNode twelve = new ListNode(5, thirteen);
+//ListNode eleven = new ListNode(5, twelve);
+//ListNode ten = new ListNode(2, eleven);
+//ListNode nine = new ListNode(4, ten);
+//ListNode eight = new ListNode(9, nine);
+//ListNode seven = new ListNode(7, eight);
+//ListNode six = new ListNode(1, seven);
+//ListNode five = new ListNode(8, six);
+//ListNode four = new ListNode(6, five);
+//ListNode three = new ListNode(2, four);
+//ListNode two = new ListNode(0, three);
+//ListNode list1 = new ListNode(3, two);
 
-            #endregion
+#endregion
 
-            var paths = new List<IList<string>>()
+var paths = new List<IList<string>>()
             {
                 new List<string>() { "London", "New York" },
                 new List<string>() { "New York", "Lima" },
                 new List<string>() { "Lima", "Sao Paulo" },
             };
-            var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
-            var resW2 = new string[]
-            {
-            
-            };
+var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
+var resW2 = new string[]
+{
 
-            var res1 = new int[] { 4, 3, 6 };
-            var res2 = new int[] { 8, 2, 6, 10 };
+};
 
-          
-            
-
-            Console.WriteLine("END");
-            Console.ReadKey();
-        }
-
-       
+var res1 = new int[] { 4, 3, 6 };
+var res2 = new int[] { 8, 2, 6, 10 };
 
 
 
 
-
-    }
-
-
-}
+Console.WriteLine("END");
+Console.ReadKey();
+Console.WriteLine("Hello, World!");
