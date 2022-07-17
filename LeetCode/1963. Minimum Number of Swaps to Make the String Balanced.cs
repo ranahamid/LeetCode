e89146b9843d3c603388e4070903a3dabbc4ba9d
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-17 14:01:36 +0600

    CanPlaceFlowers

---
+++ LeetCode/1963. Minimum Number of Swaps to Make the String Balanced.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1963
+    {
+        /// <summary>
+        /// Runtime: 105 ms, faster than 91.11% of C# online submissions for Minimum Number of Swaps to Make the String Balanced.
+        ///  Memory Usage: 69.3 MB, less than 42.22% of C# online submissions for Minimum Number of Swaps to Make the String Balanced.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int MinSwaps_(string s)
+        {
+            var counter = 0;
+            foreach (var item in s)
+            {
+                if (item == '[')
+                    counter++;
+                else
+                {
+                    if (counter > 0)
+                    {
+                        counter--;
+                    }
+                }
+            }
+            return (counter + 1) / 2;
+        }
+        /// <summary>
+        /// Runtime: 152 ms, faster than 44.44% of C# online submissions for Minimum Number of Swaps to Make the String Balanced.
+        /// Memory Usage: 70.3 MB, less than 13.33% of C# online submissions for Minimum Number of Swaps to Make the String Balanced.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int MinSwaps_2(string s)
+        {
+            var counter = 0;
+            foreach (var item in s)
+            {
+                if (item == '[')
+                    counter++;
+                else
+                {
+                    if (counter > 0)
+                    {
+                        counter--;
+                    }
+                }
+            }
+            if (counter % 2 == 0)  //can exclude these lines
+                return counter / 2; //can exclude these lines
+            else //can exclude these lines
+                return (counter + 1) / 2;
+        }
+
+    }
+}
---
+++ LeetCode/Premium/1150. Check If a Number Is Majority Element in a Sorted Array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Premium
+{
+    public class Solution
+    {
+        public static bool IsMajorityElement(int[] nums, int target)
+        {
+            return nums.Count(x => x == target) > nums.Length / 2;
+        }
+    }
+}
