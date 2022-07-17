896dfb5033c253a712f097e47d6e52e6ad7551f6
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-17 10:20:14 +0600

    d

---
+++ LeetCode/Contest 302/6120. Maximum Number of Pairs in Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Contest_302
+{
+    internal class _6120
+    {
+        public static int[] NumberOfPairs(int[] nums)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (result.ContainsKey(item))
+                {
+                    result[item]++;
+                }
+                else
+                {
+                    result[item] = 1;
+                }
+            }
+            var res1 = 0;
+            var res2 = 0;
+            foreach (var item in result)
+            {
+                res1 = res1 + (item.Value / 2);
+                if (item.Value % 2 == 1)
+                {
+                    res2++;
+                }
+            }
+            return new int[] { res1, res2 };
+        }
+    }
+}
---
+++ LeetCode/Contest 302/6164. Max Sum of a Pair With Equal Sum of Digits.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Contest_302
+{
+    internal class _6164
+    {
+        public static int MaximumSum(int[] nums)
+        {
+            int max = -1;
+            int temp = 0;
+            Dictionary<int, int> mp = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                temp = GetDigitSum(nums[i]);
+                if (mp.ContainsKey(temp))
+                {
+                    if (nums[i] + mp[temp] > max)
+                    {
+                        max = nums[i] + mp[temp];
+                    }
+                    mp[temp] = Math.Max(nums[i], mp[temp]);
+                }
+                else
+                {
+                    mp[temp] = nums[i];
+                }
+            }
+            return max;
+        }
+        public static int GetDigitSum(int source)
+        {
+            int sum = 0;
+            while (source > 0)
+            {
+                sum += (int)(source % 10);
+                source = source / 10;
+            }
+            return sum;
+        }
+
+    }
+}
