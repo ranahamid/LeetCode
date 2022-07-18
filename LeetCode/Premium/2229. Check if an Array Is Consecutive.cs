4dab4947068bcd3db2ced917d1463b562c948071
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-18 20:34:16 +0600

    s

---
+++ LeetCode/Premium/2229. Check if an Array Is Consecutive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Premium
+{
+    internal class _2229
+    {/// <summary>
+     /// Runtime: 378 ms, faster than 20.00% of C# online submissions for Check if an Array Is Consecutive.
+     /// Memory Usage: 49.2 MB, less than 60.00% of C# online submissions for Check if an Array Is Consecutive.
+     /// </summary>
+     /// <param name="nums"></param>
+     /// <returns></returns>
+        public bool IsConsecutive(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] + 1 != nums[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,69 +1,69 @@
-﻿using System.Numerics;
-using System.Text;
+﻿//using System.Numerics;
+//using System.Text;
 
-int[][] nums = new int[][]
-          {
-                new int[2] {9,4}, new int[2]{6,1}, new int[2]{3,8}, new int[2]{12,9}, new int[2]{11,4}, new int[2]{4,9}, new int[2]{2,7},
-                 new int[2]{10,3}, new int[2]{13,1}, new int[2]{13,1}, new int[2]{6,1}, new int[2]{5,10}
-          };
+//int[][] nums = new int[][]
+//          {
+//                new int[2] {9,4}, new int[2]{6,1}, new int[2]{3,8}, new int[2]{12,9}, new int[2]{11,4}, new int[2]{4,9}, new int[2]{2,7},
+//                 new int[2]{10,3}, new int[2]{13,1}, new int[2]{13,1}, new int[2]{6,1}, new int[2]{5,10}
+//          };
 
-int[][] nums1 = new int[29][]
-{
-new int[2] {22,19},new int[2] {16,17},new int[2] {10,7},new int[2] {27,16},new int[2] {9,9},new int[2] {21,4},new int[2] {24,2},new int[2] {12,11},new int[2] {2,5},new int[2] {24,12},new int[2] {25,7},new int[2] {7,13},new int[2] {14,9},new int[2] {23,15},new int[2] {18,17},new int[2] {22,16},new int[2] {4,14},new int[2] {14,17},new int[2] {25,11},new int[2] {12,16},new int[2] {29,3},new int[2] {22,11},new int[2] {29,2},new int[2] {24,2},new int[2] {24,15},new int[2] {7,14},new int[2] {7,3},new int[2] {7,14},new int[2] {1,3}
-};
+//int[][] nums1 = new int[29][]
+//{
+//new int[2] {22,19},new int[2] {16,17},new int[2] {10,7},new int[2] {27,16},new int[2] {9,9},new int[2] {21,4},new int[2] {24,2},new int[2] {12,11},new int[2] {2,5},new int[2] {24,12},new int[2] {25,7},new int[2] {7,13},new int[2] {14,9},new int[2] {23,15},new int[2] {18,17},new int[2] {22,16},new int[2] {4,14},new int[2] {14,17},new int[2] {25,11},new int[2] {12,16},new int[2] {29,3},new int[2] {22,11},new int[2] {29,2},new int[2] {24,2},new int[2] {24,15},new int[2] {7,14},new int[2] {7,3},new int[2] {7,14},new int[2] {1,3}
+//};
 
 
-#region node
+//#region node
 
-//ListNode eig = new ListNode(3);
-//TreeNode sev = new TreeNode(4);
-//TreeNode six = new TreeNode(3);
-//TreeNode five = new TreeNode(2,null,sev);
-//TreeNode list2 = new TreeNode(1, five, six);
+////ListNode eig = new ListNode(3);
+////TreeNode sev = new TreeNode(4);
+////TreeNode six = new TreeNode(3);
+////TreeNode five = new TreeNode(2,null,sev);
+////TreeNode list2 = new TreeNode(1, five, six);
 
-//ListNode fifteen = new ListNode(0);
-//ListNode fourtenn = new ListNode(0, fifteen);
+////ListNode fifteen = new ListNode(0);
+////ListNode fourtenn = new ListNode(0, fifteen);
 
-//ListNode thirteen = new ListNode(0);
-//ListNode twelve = new ListNode(5, thirteen);
-//ListNode eleven = new ListNode(5, twelve);
-//ListNode ten = new ListNode(2, eleven);
-//ListNode nine = new ListNode(4, ten);
-//ListNode eight = new ListNode(9, nine);
-//ListNode seven = new ListNode(7, eight);
-//ListNode six = new ListNode(1, seven);
-//ListNode five = new ListNode(8, six);
-//ListNode four = new ListNode(6, five);
-//ListNode three = new ListNode(2, four);
-//ListNode two = new ListNode(0, three);
-//ListNode list1 = new ListNode(3, two);
+////ListNode thirteen = new ListNode(0);
+////ListNode twelve = new ListNode(5, thirteen);
+////ListNode eleven = new ListNode(5, twelve);
+////ListNode ten = new ListNode(2, eleven);
+////ListNode nine = new ListNode(4, ten);
+////ListNode eight = new ListNode(9, nine);
+////ListNode seven = new ListNode(7, eight);
+////ListNode six = new ListNode(1, seven);
+////ListNode five = new ListNode(8, six);
+////ListNode four = new ListNode(6, five);
+////ListNode three = new ListNode(2, four);
+////ListNode two = new ListNode(0, three);
+////ListNode list1 = new ListNode(3, two);
 
-#endregion
+//#endregion
 
-var paths = new List<IList<string>>()
-            {
-                new List<string>() { "London", "New York" },
-                new List<string>() { "New York", "Lima" },
-                new List<string>() { "Lima", "Sao Paulo" },
-            };
-var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
-var resW2 = new string[]
-{
+//var paths = new List<IList<string>>()
+//            {
+//                new List<string>() { "London", "New York" },
+//                new List<string>() { "New York", "Lima" },
+//                new List<string>() { "Lima", "Sao Paulo" },
+//            };
+//var resW1 = new char[] { 'z', 'a', 'b', 'c', 'x', 'x', 'x' };
+//var resW2 = new string[]
+//{
   
-};
+//};
 
-var res1 = new int[] { 2, 7, 11, 15 };
-var res2 = new int[] { 8, 2, 6, 10 };
-Solution solution = new Solution();
-Console.WriteLine(solution.FirstUniqChar("leetcode"));
+//var res1 = new int[] { 2, 7, 11, 15 };
+//var res2 = new int[] { 8, 2, 6, 10 };
+//Solution solution = new Solution();
+//Console.WriteLine(solution.FirstUniqChar("leetcode"));
 
 
 
-Console.WriteLine("END");
-Console.ReadKey();
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("END");
+//Console.ReadKey();
+//Console.WriteLine("Hello, World!");
 
-public class Solution
-{
-
-}
+//public class Solution
+//{
+    
+//}
