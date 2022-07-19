14c1d22364c8ed2d61cab390aa451b4c8491a269
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-19 15:46:01 +0600

    s

---
+++ LeetCode/Premium/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,11 @@
+﻿select distinct employee_id , department_id 
+from Employee 
+where primary_flag ='Y'
+
+UNION 
+
+select distinct employee_id , department_id 
+from Employee 
+group by employee_id 
+having count(employee_id)=1 
+
