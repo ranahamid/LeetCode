25462163d7dbedf30a1144a382b863ba68d04a26
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-30 16:12:34 +0600

    faster

---
+++ LeetCode/Premium/619. Biggest Single Number.sql
@@ -0,0 +1,18 @@
+﻿select  max(num) as num  
+from (
+
+   select num
+    from mynumbers
+group by num
+having count(num)=1
+)as tables
+
+
+--mysql
+--Runtime: 897 ms, faster than 11.77% of MySQL online submissions for Biggest Single Number.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Biggest Single Number.
+
+
+--sql server
+--Runtime: 439 ms, faster than 98.35% of MS SQL Server online submissions for Biggest Single Number.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Biggest Single Number.
