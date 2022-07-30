6bf7c40c43093229f61f636145cebbed4e58d00d
Rana Hamid <rhamid@mymedicalhub.com>
2022-07-30 23:13:06 +0600

    d

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -66,9 +66,20 @@ Console.WriteLine(s.DayOfYear("2016-02-09"));
 //Console.ReadKey();
 
 Console.WriteLine("Hello, World!");
-
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class Solution
 {
-
+   
 }
