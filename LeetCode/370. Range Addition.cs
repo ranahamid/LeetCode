a36561c2b4ecbab1b59c8041912fc3671bf179af
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-03 15:41:42 +0600

    d

---
+++ LeetCode/370. Range Addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _370
+    {
+        public int[] GetModifiedArray_TLE(int length, int[][] updates)
+        {
+            var arr = new int[length];
+            foreach (var item in updates)
+            {
+                var start = item[0];
+                var end = item[1];
+                var val = item[2];
+                for (int i = start; i <= end; i++)
+                {
+                    arr[i] += val;
+                }
+            }
+            return arr;
+        }
+    }
+}
