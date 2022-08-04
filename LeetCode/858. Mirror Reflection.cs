6d6ed9800ce612f3852491fe607bac4c2e83a2d9
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-04 09:11:30 +0600

    d

---
+++ LeetCode/858. Mirror Reflection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _858
+    {
+        public int MirrorReflection(int p, int q)
+        {
+            while (p % 2 == 0 && q % 2 == 0)
+            {
+                p>>=1;
+                q>>= 1;
+            }
+            if (p % 2 == 0 && q % 2 == 1)
+                return 2;
+            if (p % 2 == 1 && q % 2 == 1)
+                return 1;
+            return 0;
+        }
+        /// <summary>
+        /// Runtime: 35 ms, faster than 77.78% of C# online submissions for Mirror Reflection.
+        /// Memory Usage: 25.2 MB, less than 11.11% of C# online submissions for Mirror Reflection.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public int MirrorReflection_1(int p, int q)
+        {
+            while (p % 2 != 1 && q % 2 != 1)
+            {
+                p = p / 2;
+                q = q / 2;
+            }
+            if (p % 2 == 0 && q % 2 == 1)
+                return 2;
+            if (p % 2 == 1 && q % 2 == 1)
+                return 1;
+            return 0;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -78,20 +78,21 @@ var res2 = new int[] { 14, 21, 8, 35, 30, 21, 28, 19, 10, 25, 16, 23, 14, 13, 0,
 
 
 Solution solution = new Solution();
-Console.WriteLine(solution.DivisorSubstrings(430043, 2));
+Console.WriteLine(solution.MirrorReflection(3, 1));
 
 public class Solution
 {
+   
     //https://leetcode.com/problems/random-pick-index/
     private List<int> list;
-    public Solution(int[] nums)
-    {
-        list = new List<int>(nums);
-    }
+    //public Solution(int[] nums)
+    //{
+    //    list = new List<int>(nums);
+    //}
 
     public int Pick(int target)
     {
-        
+        return 0;
     }
 }
 
