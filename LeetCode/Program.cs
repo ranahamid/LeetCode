53d20976da36e49200728836ed32978c48ed0906
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-04 21:10:53 +0600

    c

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -100,6 +100,22 @@ Console.WriteLine(solution.NewInteger(9));
 
 public class Solution
 {
-  
+    /// <summary>
+    /// Runtime: 102 ms, faster than 100.00% of C# online submissions for Chalkboard XOR Game.
+    /// Memory Usage: 42.3 MB, less than 14.29% of C# online submissions for Chalkboard XOR Game
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool XorGame(int[] nums)
+    {
+        if(nums.Length%2==0)
+            return true;
+        var result = 0;
+        foreach(var x in nums)
+        {
+            result = result ^ x;
+        }
+        return result == 0;
+    }
 }
 
