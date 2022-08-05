3c5d3b23522e09c614a3ffcc53010405178d33c8
RanaHamid <ranahamid007@gmail.com>
2022-08-05 22:40:23 +0600

    MinimumMoves

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -83,10 +83,10 @@ var res2 = new int[] { 14, 21, 8, 35, 30, 21, 28, 19, 10, 25, 16, 23, 14, 13, 0,
 
 
 Solution solution = new Solution();
-Console.WriteLine(solution.FindAnagrams("abab", "ab"));
+Console.WriteLine(solution.MinimumMoves("XXX"));
 
 public class Solution
 {
-     
+    
 }
 
