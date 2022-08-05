5a7b61616d818ffc9e341655de7080d89053b787
RanaHamid <ranahamid007@gmail.com>
2022-08-05 22:33:48 +0600

    Solution

--- LeetCode/692. Top K Frequent Words.cs
+++ LeetCode/692. Top K Frequent Words.cs
@@ -9,6 +9,18 @@ namespace LeetCode
     internal class _692
     {
         /// <summary>
+        /// Runtime: 229 ms, faster than 61.54% of C# online submissions for Top K Frequent Words.
+        /// Memory Usage: 47.3 MB, less than 24.79% of C# online submissions for Top K Frequent Words.
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public IList<string> TopKFrequent_LINQ(string[] words, int k)
+        {
+            return words.GroupBy(x => x).OrderByDescending(x => x.Count()).ThenBy(x => x.Key).Take(k).Select(x => x.Key).ToList();
+        }
+
+        /// <summary>
         /// Runtime: 290 ms, faster than 17.95% of C# online submissions for Top K Frequent Words.
         /// Memory Usage: 46.6 MB, less than 83.33% of C# online submissions for Top K Frequent Words.
         /// </summary>
---
+++ LeetCode/796. Rotate String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _796
+    {
+        /// <summary>
+        /// Runtime: 120 ms, faster than 29.44% of C# online submissions for Rotate String.
+        /// Memory Usage: 36.4 MB, less than 80.37% of C# online submissions for Rotate String.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="goal"></param>
+        /// <returns></returns>
+        public bool RotateString(string s, string goal)
+        {
+            return (s + s).Contains(goal) && s.Count() == goal.Count();
+        }
+    }
+}
