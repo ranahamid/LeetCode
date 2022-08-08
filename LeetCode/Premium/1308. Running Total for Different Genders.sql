02566244b9cb1d5dc4419ad1d6039cb60a57c8c1
RanaHamid <ranahamid007@gmail.com>
2022-08-08 15:44:46 +0600

    f

---
+++ LeetCode/Premium/1308. Running Total for Different Genders.sql
@@ -0,0 +1,20 @@
+﻿select gender, day, sum(score_points ) over (partition by gender order by day) as total 
+from Scores  
+
+
+--mysql 
+--Runtime: 1602 ms, faster than 81.60% of MS SQL Server online submissions for Running Total for Different Genders.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Running Total for Different Genders.
+
+--SQL Server.
+--Runtime: 3137 ms, faster than 12.00% of MS SQL Server online submissions for Running Total for Different Genders.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Running Total for Different Genders.
+
+select gender, day, (select sum(score_points ) 
+from Scores  s1
+where s1.gender=s2.gender AND s1.day<=s2.day) as total from Scores as s2
+group by gender , day
+order by gender ,day 
+
+--Runtime: 1850 ms, faster than 64.00% of MS SQL Server online submissions for Running Total for Different Genders.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Running Total for Different Genders.
---
+++ LeetCode/Premium/2298. Tasks Count in the Weekend.sql
@@ -0,0 +1,21 @@
+﻿select 
+sum(case WHEN DAYOFWEEK(submit_date ) in (1,7) then 1 else 0  end)as weekend_cnt,
+sum(case WHEN DAYOFWEEK(submit_date ) in (2,3,4,5,6) then 1 else 0  end)as working_cnt 
+from Tasks 
+
+--my sql 
+--Runtime: 337 ms, faster than 96.59% of MySQL online submissions for Tasks Count in the Weekend.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Tasks Count in the Weekend.
+
+
+
+
+
+select 
+sum(case WHEN DATEPART(dw,submit_date ) in (1,7) then 1 else 0  end)as weekend_cnt,
+sum(case WHEN DATEPART(dw,submit_date ) in (2,3,4,5,6) then 1 else 0  end)as working_cnt 
+from Tasks 
+
+--MS SQL SERVER
+--Runtime: 706 ms, faster than 80.00% of MS SQL Server online submissions for Tasks Count in the Weekend.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Tasks Count in the Weekend.
