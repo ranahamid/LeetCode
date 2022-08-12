58e41395e7a01fc77ec509fc8386a4b1bd529e4e
RanaHamid <ranahamid007@gmail.com>
2022-08-12 22:12:38 +0600

    d

--- LeetCode/1426. Counting Elements.cs
+++ LeetCode/1426. Counting Elements.cs
@@ -9,8 +9,8 @@ namespace LeetCode
     internal class _1426
     {
         /// <summary>
-     /// Runtime: 144 ms, faster than 60.00% of C# online submissions for Counting Elements.
-     /// Memory Usage: 37.3 MB, less than 60.00% of C# online submissions for Counting Elements.
+        /// Runtime: 94 ms, faster than 80.00% of C# online submissions for Counting Elements.
+        /// Memory Usage: 38.3 MB, less than 60.00% of C# online submissions for Counting Elements.
      /// </summary>
      /// <param name="arr"></param>
      /// <returns></returns>
