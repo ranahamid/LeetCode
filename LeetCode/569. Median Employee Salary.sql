4688dfa84a659c2f4ffdbe0eca85b81663adb469
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-13 23:12:11 +0600

    d

---
+++ LeetCode/569. Median Employee Salary.sql
@@ -0,0 +1,18 @@
+﻿with cte as
+(
+    select *, row_number() over(partition by company  order by salary) as row_id,
+    count(salary) over(partition by company) as salaryCount
+    from Employee
+)
+select id, company, salary 
+from cte 
+where row_id between salaryCount/2.0 and salaryCount/2.0+1
+
+--ms sql server
+--Runtime: 1108 ms, faster than 73.78% of MS SQL Server online submissions for Median Employee Salary.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Median Employee Salary.
+
+
+--mysql
+--Runtime: 277 ms, faster than 92.20% of MySQL online submissions for Median Employee Salary.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Median Employee Salary.
---
+++ LeetCode/571. Find Median Given Frequency of Numbers.sql
@@ -0,0 +1,19 @@
+﻿WITH TEMP AS(
+select num,
+    sum(frequency ) over(order by num) as low,
+   
+     sum(frequency ) over(order by num desc) as high,
+    sum(frequency) over() as total
+    from Numbers
+)
+select avg(num*1.0) as median 
+from TEMP 
+where 2*low>=total AND 2*high>=total
+
+--sql server
+--Runtime: 608 ms, faster than 85.44% of MS SQL Server online submissions for Find Median Given Frequency of Numbers.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Find Median Given Frequency of Numbers.
+
+--mysql
+--Runtime: 262 ms, faster than 66.31% of MySQL online submissions for Find Median Given Frequency of Numbers.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Median Given Frequency of Numbers.
