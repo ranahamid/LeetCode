2df7cfe95652fb6a9cc690bf425aecb95ece638c
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-13 23:31:46 +0600

    d

---
+++ LeetCode/185. Department Top Three Salaries.sql
@@ -0,0 +1,18 @@
+﻿select d.name  as Department ,e.name  as Employee , salary  as Salary 
+from  Department  as d,Employee as e
+WHERE d.id =e.departmentId AND 
+(
+    select count(distinct  salary)
+    from Employee 
+    where departmentId = d.id AND 
+    salary > e.salary 
+)<3
+order by salary desc
+
+--sql server
+--Runtime: 774 ms, faster than 71.82% of MS SQL Server online submissions for Department Top Three Salaries.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Department Top Three Salaries.
+
+--my sql
+--Runtime: 1021 ms, faster than 55.12% of MySQL online submissions for Department Top Three Salaries.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Department Top Three Salaries.
