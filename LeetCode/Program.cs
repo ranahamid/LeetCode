1552dd02b7040c3684a71372604f57311f28cf02
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-17 12:22:43 +0600

    Solution

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -104,23 +104,7 @@ public class TreeNode
 }
 public class Solution
 {
-    public int FindMaxConsecutiveOnes(int[] nums, int k)
-    {
-        int left = 0, right = 0;
-        for( right = 0; right < nums.Length; right++)
-        {
-            if (nums[right] == 0)
-            {
-                k--;
-            }
-            if (k < 0)
-            {
-                k = k + 1 - nums[left];
-                left++;
-            }
-        }
-        return right - left;
-    }
+  
 }
 
 public static class Helper
