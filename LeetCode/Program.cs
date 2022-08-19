cd75217e6042e1d3a43c4d6dead389a8ff841734
RanaHamid <ranahamid007@gmail.com>
2022-08-19 22:44:09 +0600

    d

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -86,9 +86,9 @@ int[][] nums = new int[][]
           };
 Solution solution = new Solution();
 
-var res1 = new int[] { 2, 3, 1, 1, 4 };
+var res1 = new int[] {999, 997, 980, 976, 948, 940, 938, 928, 924, 917, 907, 907, 881, 878, 864, 862, 859, 857, 848, 840, 824, 824, 824, 805, 802, 798, 788, 777, 775, 766, 755, 748, 735, 732, 727, 705, 700, 697, 693, 679, 676, 644, 634, 624, 599, 596, 588, 583, 562, 558, 553, 539, 537, 536, 509, 491, 485, 483, 454, 449, 438, 425, 403, 368, 345, 327, 287, 285, 270, 263, 255, 248, 235, 234, 224, 221, 201, 189, 187, 183, 179, 168, 155, 153, 150, 144, 107, 102, 102, 87, 80, 57, 55, 49, 48, 45, 26, 26, 23, 15 };
 var res2 = new int[] { 10, 10, 1 };
-Console.WriteLine(solution.Jump(res1));
+Console.WriteLine(solution.MaximumDifference(res1));
 public class TreeNode
 {
     public int val;
@@ -102,8 +102,8 @@ public class TreeNode
     }
 }
 public class Solution
-{
-  
+{    
+    
 }
 
 public static class Helper
