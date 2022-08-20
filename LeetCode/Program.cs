1537b14402cd2df0dded9869c7a65347fb9c0b64
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-20 10:53:13 +0600

    Solution

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -74,25 +74,43 @@ var resW2 = new string[]
 
 int[][] nums = new int[][]
           {
-               new int[] {0,0,0,0,0,0,0,0},
-              //  new int[] {0,0,1,0,0,0,0,1,0,0,0,0,0},
-              //  new int[]{0,0,0,0,0,0,0,1,1,1,0,0,0},
-              //new int[]{0,1,1,0,1,0,0,0,0,0,0,0,0},
-              //  new int[]{0,1,0,0,1,1,0,0,1,0,1,0,0},
-              //new int[]{0,1,0,0,1,1,0,0,1,1,1,0,0},
-              // new int[]{0,0,0,0,0,0,0,0,0,0,1,0,0},
-              //new int[]{0,0,0,0,0,0,0,1,1,1,0,0,0},
-              //new int[]{0,0,0,0,0,0,0,1,1,0,0,0,0},
+               new int[] {10,60},
+              new int[] {20,30},
+              new int[] {30,30},
+              new int[] {60,40},
           };
 Solution solution = new Solution();
 
-var res1 = new int[] { 999, 997, 980, 976, 948, 940, 938, 928, 924, 917, 907, 907, 881, 878, 864, 862, 859, 857, 848, 840, 824, 824, 824, 805, 802, 798, 788, 777, 775, 766, 755, 748, 735, 732, 727, 705, 700, 697, 693, 679, 676, 644, 634, 624, 599, 596, 588, 583, 562, 558, 553, 539, 537, 536, 509, 491, 485, 483, 454, 449, 438, 425, 403, 368, 345, 327, 287, 285, 270, 263, 255, 248, 235, 234, 224, 221, 201, 189, 187, 183, 179, 168, 155, 153, 150, 144, 107, 102, 102, 87, 80, 57, 55, 49, 48, 45, 26, 26, 23, 15 };
+var res1 = new int[] { };
 var res2 = new int[] { 10, 10, 1 };
-Console.WriteLine(solution.LargestInteger(1234));
+Console.WriteLine(solution.MinRefuelStops(100, 10, nums));
 
 public class Solution
 {
-    
+    public int MinRefuelStops(int target, int tank, int[][] stations)
+    {
+        int result = 0, prev = 0;
+        var queue = new PriorityQueue<int, int>();        
+        for (int i = 0; i <= stations.Length; i++)
+        {
+            int location = i<stations.Length? stations[i][0]:target;
+            int capacity = i < stations.Length ? stations[i][1]: 0;
+            tank = tank - (location - prev);
+            while (queue.Count > 0 && tank < 0)
+            {
+                result++; 
+                tank += queue.Dequeue();
+            }
+            if (tank < 0)
+                return -1;
+            if(i < stations.Length)
+            {
+                queue.Enqueue(capacity, -capacity);
+                prev = location;
+            }         
+        } 
+        return result;
+    }
 }
 
 public static class Helper
