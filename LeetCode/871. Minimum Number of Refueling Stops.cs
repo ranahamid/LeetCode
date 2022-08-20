624e3039d5f07e7c4adb3de935b8c0708cb277eb
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-20 10:38:02 +0600

    MinRefuelStops

---
+++ LeetCode/871. Minimum Number of Refueling Stops.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _871
+    {
+        /// <summary>
+        /// Runtime: 133 ms, faster than 78.13% of C# online submissions for Minimum Number of Refueling Stops.
+        /// Memory Usage: 39.6 MB, less than 71.88% of C# online submissions for Minimum Number of Refueling Stops.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="tank"></param>
+        /// <param name="stations"></param>
+        /// <returns></returns>
+        public int MinRefuelStops(int target, int tank, int[][] stations)
+        {
+            var result = 0;
+            var prev = 0;
+            var queue = new PriorityQueue<int, int>();
+            int location = 0, capacity = 0;
+            for (int i = 0; i < stations.Length; i++)
+            {
+                location = stations[i][0];
+                capacity = stations[i][1];
+                tank = tank - (location - prev);
+                while (queue.Count > 0 && tank < 0)
+                {
+                    result++;
+                    var val = queue.Dequeue();
+                    tank += val;
+                }
+                if (tank < 0)
+                    return -1;
+                queue.Enqueue(capacity, -capacity);
+                prev = location;
+            }
+            //last position 
+            tank = tank - (target - prev);
+            while (queue.Count > 0 && tank < 0)
+            {
+                result++;
+                var val = queue.Dequeue();
+                tank += val;
+            }
+            if (tank < 0)
+                return -1;
+            return result;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 
-
 int[][] nums2 = new int[][]
           {
                 new int[2] {0,1},
@@ -90,20 +89,10 @@ Solution solution = new Solution();
 var res1 = new int[] { 999, 997, 980, 976, 948, 940, 938, 928, 924, 917, 907, 907, 881, 878, 864, 862, 859, 857, 848, 840, 824, 824, 824, 805, 802, 798, 788, 777, 775, 766, 755, 748, 735, 732, 727, 705, 700, 697, 693, 679, 676, 644, 634, 624, 599, 596, 588, 583, 562, 558, 553, 539, 537, 536, 509, 491, 485, 483, 454, 449, 438, 425, 403, 368, 345, 327, 287, 285, 270, 263, 255, 248, 235, 234, 224, 221, 201, 189, 187, 183, 179, 168, 155, 153, 150, 144, 107, 102, 102, 87, 80, 57, 55, 49, 48, 45, 26, 26, 23, 15 };
 var res2 = new int[] { 10, 10, 1 };
 Console.WriteLine(solution.LargestInteger(1234));
-public class TreeNode
-{
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-    {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+
 public class Solution
-{ 
+{
+    
 }
 
 public static class Helper
@@ -198,5 +187,17 @@ public static class Helper
             source = source / 10;
         }
         return digits;
+    }
+}
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
