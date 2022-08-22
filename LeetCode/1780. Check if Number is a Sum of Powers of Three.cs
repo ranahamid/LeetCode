5aabb7898daf8ff4c46da8a4299a439a94ea91b2
RanaHamid <ranahamid007@gmail.com>
2022-08-22 21:28:42 +0600

    d

---
+++ LeetCode/1780. Check if Number is a Sum of Powers of Three.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1780
+    {
+        /// <summary>
+        /// Runtime: 33 ms, faster than 100.00% of C# online submissions for Check if Number is a Sum of Powers of Three.
+        /// Memory Usage: 27.3 MB, less than 57.14% of C# online submissions for Check if Number is a Sum of Powers of Three.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool CheckPowersOfThree(int n)
+        {
+            double sum = n;
+            for (int i = 16; i >= 0; i--)
+            {
+                var div = Math.Pow(3, i);
+                if (sum == 0)
+                    return true;
+                if (sum - div >= 0)
+                {
+                    sum -= div;
+                }
+            }
+            if (sum == 0)
+                return true;
+            return false;
+        }
+    }
+}
