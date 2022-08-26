1f1838b8ee5c9cba96328098dc55a7e3d71672ce
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-26 14:53:13 +0600

    d

---
+++ LeetCode/CodeSignal/adjacent elements that has the largest product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CodeSignal
+{
+    internal class adjacent_elements_that_has_the_largest_product
+    {
+        int solution(int[] inputArray)
+        {
+            var max = Int32.MinValue;
+            for(int i=0; i < inputArray.Length-1; i++)
+            {
+                max = Math.Max(max, inputArray[i] * inputArray[i + 1]);
+            }
+            return max;
+        }
+
+    }
+}
---
+++ LeetCode/CodeSignal/palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CodeSignal
+{
+    internal class palindrome
+    {
+        bool solution(string inputString)
+        {
+            int i = 0;
+            int j = inputString.Length - 1;
+            while (j >= 0)
+            {
+                if (inputString[i] != inputString[j])
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+    }
+}
