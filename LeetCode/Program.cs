02a161b36527df38632debee805531d8bdae909e
Rana Hamid <rhamid@mymedicalhub.com>
2022-08-27 19:33:59 +0600

    1

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -89,8 +89,39 @@ Console.WriteLine(solution.solution("foo(bar(baz))blim")); //20
 
 public class Solution
 {
-  
+    public int MaxSumSubmatrix(int[][] matrix, int k)
+    {
+        var max = int.MinValue;
+        int rows = matrix.Length;
+        int cols= matrix[0].Length;
+        int[] sum=new int[rows];
 
+        for(int left=0; left< cols; left++)
+        {
+            Array.Clear(sum, 0, rows);
+            for(int right = left; right < cols; right++)
+            {
+                for(int i = 0; i < rows; i++)
+                {
+                    sum[i] += matrix[i][right];
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    int total = 0;
+                    for(int j = i; j < rows; j++)
+                    {
+                        total += sum[j];
+                        if (total == k)
+                            return k;
+                        if(total>max && total <= k)
+                            max = total;
+                    }
+                }
+            }
+        }
+        return max;
+    }
 }
 
 public static class Helper
