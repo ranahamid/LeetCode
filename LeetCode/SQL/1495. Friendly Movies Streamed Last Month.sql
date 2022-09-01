0d0c2370e2277072596bd33b0dbf0903da18d776
Rana Hamid <ranahamid007@gmail.com>
2022-09-01 22:56:27 +0600

    d

---
+++ LeetCode/SQL/1495. Friendly Movies Streamed Last Month.sql
@@ -0,0 +1,11 @@
+﻿select distinct title 
+from content 
+where Kids_content  ='Y' AND content_type  ='Movies'
+AND content_id in (
+select content_id
+    from TVProgram 
+    where program_date between '2020-06-01' AND '2020-06-30 ' 
+)
+
+--Runtime: 537 ms, faster than 91.67% of MS SQL Server online submissions for Friendly Movies Streamed Last Month.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Friendly Movies Streamed Last Month.
