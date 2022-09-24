14cc6815aa7c32334347556abbd9d64274d2fc97
Rana Hamid <rhamid@mymedicalhub.com>
2022-09-24 23:28:27 +0600

    Solution

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -96,39 +96,7 @@ Solution solution = new Solution();
 Console.WriteLine(solution.ConcatenatedBinary(1));
 public class Solution
 {
-    public IList<IList<int>> RemoveInterval(int[][] intervals, int[] toBeRemoved)
-    {
-        List<IList<int>> result = new List<IList<int>>();
-        foreach (var interval in intervals)
-        {
-            if (interval[0] < toBeRemoved[1] || interval[1] > toBeRemoved[0])
-            {
-                result.Add(new List<int>{
-                    interval[0],interval[1]
-                });
-            }
-            else
-            {
-                if (interval[0] < toBeRemoved[0])
-                {
-
-                    result.Add(new List<int>{
-                    interval[0],toBeRemoved[0]
-                });
-                }
-
-                if (interval[1] > toBeRemoved[1])
-                {
-
-                    result.Add(new List<int>{
-                    toBeRemoved[1],interval[1]
-                });
-                }
-
-            }
-        }
-        return result;
-    }
+    
 }
 public static class Helper
 {
