6e748a9e0087941627f3f04f0a83f5e843dc0e5c
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-09 14:45:01 +0600

    FullJustify

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -96,7 +96,48 @@ Solution s = new Solution();
 Console.WriteLine(s.FullJustify(resW2,16));
 public class Solution
 {
-    
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        var numOfLetters = 0;
+        int index = 1;
+        var result = new List<string>();
+        var current = new List<StringBuilder>();
+        foreach (var word in words)
+        {
+            if (numOfLetters + current.Count + word.Length > maxWidth)
+            {
+                var remaining = maxWidth - numOfLetters;
+                for(int i = 0; i < remaining; i++)
+                {
+                    if (current.Count == 1)
+                        current[0].Append('*');
+                    else
+                    {
+                        var ind = i % (current.Count - 1);
+                        current[ind].Append('*');
+                    }
+                }
+                var sentence = string.Join("", current);
+                result.Add(sentence);
+                current = new List<StringBuilder>();
+                numOfLetters = 0;
+            }
+            current.Add(new StringBuilder(word));
+            numOfLetters += word.Length;
+            if (index == words.Length)
+            { 
+                var final = string.Join(" ", current);
+                var remaining = maxWidth - final.Length;
+                for (int i = 0; i < remaining; i++)                   
+                        final += "*";
+                result.Add(final);
+            }
+            index++;
+        }
+        foreach (var item in result)
+            Console.WriteLine(item);
+        return result;
+    }
 }
 
 public static class Helper
