0e0e68c1bb0acc0e98ba63715694e0034f9f28d1
Rana Hamid <ranahamid007@gmail.com>
2022-10-14 16:31:45 +0600

    IsRectangleOverlap

---
+++ LeetCode/836. Rectangle Overlap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _836
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+            if (rec1[0] >= rec2[2] || rec1[1] >= rec2[3] ||
+            rec2[0] >= rec1[2] || rec2[1] >= rec1[3])
+                return false;
+            return true;
+        }
+    }
+}
