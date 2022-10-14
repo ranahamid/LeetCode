039d60b7134ba604e8ff6080a24b63b332f0d8fe
Rana Hamid <ranahamid007@gmail.com>
2022-10-14 18:57:16 +0600

    d

---
+++ LeetCode/1114. Print in Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Foo
+    {
+        int counter = 0;
+        public Foo()
+        {
+            counter = 0;
+        }
+
+        public void First(Action printFirst)
+        {
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+            counter++;
+        }
+
+        public void Second(Action printSecond)
+        {
+            while (counter < 1)
+                continue;
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+            counter++;
+        }
+
+        public void Third(Action printThird)
+        {
+            while (counter < 2)
+                continue;
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+}
