c502bfb659b7ceaaef569b65e5edc787d68f6dd1
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-16 17:56:09 +0600

    LargestTriangleArea

---
+++ LeetCode/455. Assign Cookies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _455
+    {
+        /// <summary>
+        /// Runtime 201 ms Beats 47.20% Memory 41.7 MB Beats 83.20%
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int counter = 0;
+
+            foreach (var item in s)
+            {
+                if (counter >= g.Length)
+                    break;
+                if (g[counter] <= item)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ LeetCode/Premium/252. Meeting Rooms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Premium
+{
+    internal class _252
+    {
+        /// <summary>
+        /// Runtime 193 ms Beats 34.11% Memory 41.5 MB Beats 38.76%
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            intervals = intervals.OrderBy(x => x[0]).ToArray();
+            for (int i = 0; i < intervals.Length - 1; i++)
+            {
+                //make sure that each meeting ends before the next one starts.
+                if (intervals[i][1] > intervals[i + 1][0])
+                    return false;
+            }
+            return true;
+        }
+        #region brute force 
+        /// <summary>
+        /// Runtime 177 ms Beats 59.30% Memory 41.5 MB Beats 38.76%
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
+        public bool CanAttendMeetings_bruteforce(int[][] intervals)
+        {
+            var result = new bool[1000000];
+            foreach (var item in intervals)
+            {
+                for (int i = item[0]; i < item[1]; i++)
+                {
+                    if (result[i])
+                        return false;
+                    result[i] = true;
+                }
+            }
+            return true;
+        }
+        #endregion
+    }
+}
