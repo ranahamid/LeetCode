319ad6560ae8788a144fcb66efc1cda0dfaff9ef
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-17 10:48:07 +0600

    d

---
+++ LeetCode/1759. Count Number of Homogenous Substrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1759
+    {
+        /// <summary>
+        /// Runtime 90 ms Beats 77.78% Memory 40.6 MB Beats 16.67
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int CountHomogenous(string s)
+        {
+            int result = 0, current = 0, counter = 0;
+            var mod = (int)(Math.Pow(10, 9) + 7);
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                if (s[i] == current)
+                    counter++;
+                else
+                    counter = 1;
+                current = s[i];
+                result = (result + counter) % mod;
+            }
+            return result;
+        }
+    }
+}
--- LeetCode/1832 Check if the Sentence Is Pangram.cs
+++ LeetCode/1832 Check if the Sentence Is Pangram.cs
@@ -9,12 +9,33 @@ namespace LeetCode
     internal class _1832_Check_if_the_Sentence_Is_Pangram
     {
         /// <summary>
+        /// Runtime 113 ms Beats 67.7% Memory 36.5 MB Beats 84.15%
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
+        public bool CheckIfPangram(string sentence)
+        {
+            bool[] result = new bool[26];
+            foreach (var item in sentence)
+            {
+
+                result[item - 97] = true;
+            }
+
+            foreach (var item in result)
+            {
+                if (!item)
+                    return false;
+            }
+            return true;
+        }
+        /// <summary>
         /// Runtime: 115 ms, faster than 24.50% of C# online submissions for Check if the Sentence Is Pangram.
         /// Memory Usage: 36.6 MB, less than 50.99% of C# online submissions for Check if the Sentence Is Pangram.
         /// </summary>
         /// <param name="sentence"></param>
         /// <returns></returns>
-        public static bool CheckIfPangram(string sentence)
+        public static bool CheckIfPangram_Int(string sentence)
         {
             int[] result = new int[26];
             var data = sentence.ToArray();
