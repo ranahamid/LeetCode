9f6f676c156eb0a577e2bf41be2e8ec5133d73c5
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-18 10:30:27 +0600

    s

---
+++ LeetCode/Premium/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,18 @@
+﻿ select m.employee_id ,m.name , count(e.employee_id)as reports_count, ROUND(avg(e.age )) as average_age
+from Employees as m 
+inner join Employees as e on m.employee_id = e.reports_to 
+group by employee_id 
+order by employee_id 
+
+--MY SQL 
+--Runtime 685 ms Beats 80.40%
+
+
+select m.employee_id ,m.name , count(e.employee_id)as reports_count, Round(avg(e.age*1.0 ),0) as average_age
+from Employees as m 
+inner join Employees as e on m.employee_id = e.reports_to 
+group by m.employee_id, m.name 
+order by m.employee_id 
+
+--SQL Server
+--Runtime 1276 ms Beats 93.10%
