0ba75187d42978380995ec3cd6469f3c9646e5c7
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-23 22:24:18 +0600

    Calculate

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,6 +6,7 @@
 //Console.WriteLine(strList.Aggregate((s1, s2) => s1 + ", " + s2));
 
 
+using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
 int[][] nums2 = new int[][]
@@ -90,24 +91,9 @@ var res2 = new int[] { 4, 2, 8, 1, 3 };
 
 
 Solution s = new Solution();
-Console.WriteLine(s.MinCost(res1, res2));
-public class Solution
-{
-    Dictionary<Node, Node> dic = new Dictionary<Node, Node>();
-    public Node CopyRandomList(Node head)
-    {
-        if (head == null)
-            return head;
-        if (dic.ContainsKey(head))
-            return dic[head];
-        var node = new Node(head.val);
-        dic.TryAdd(head, node);
-        node.next = CopyRandomList(head.next);
-        node.random = CopyRandomList(head.random);
-        return node;
-    }
-}
+Console.WriteLine(s.Calculate("3+2*2"));
 
+ 
 
 
 
@@ -237,3 +223,4 @@ public class Node
         next = null;
         random = null;
     }
+}
