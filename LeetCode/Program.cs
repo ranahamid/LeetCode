64d4a3ad7a921f735dc6092f149420b43258a38b
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-23 21:52:16 +0600

    r

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,6 +6,8 @@
 //Console.WriteLine(strList.Aggregate((s1, s2) => s1 + ", " + s2));
 
 
+using System.Xml.Linq;
+
 int[][] nums2 = new int[][]
           {
                 new int[] {1,3},
@@ -91,7 +93,19 @@ Solution s = new Solution();
 Console.WriteLine(s.MinCost(res1, res2));
 public class Solution
 {
-    
+    Dictionary<Node, Node> dic = new Dictionary<Node, Node>();
+    public Node CopyRandomList(Node head)
+    {
+        if (head == null)
+            return head;
+        if (dic.ContainsKey(head))
+            return dic[head];
+        var node = new Node(head.val);
+        dic.TryAdd(head, node);
+        node.next = CopyRandomList(head.next);
+        node.random = CopyRandomList(head.random);
+        return node;
+    }
 }
 
 
@@ -211,3 +225,15 @@ public class TreeNode
         this.right = right;
     }
 }
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
