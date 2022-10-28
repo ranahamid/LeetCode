8c2638140244aaf06b8d2a48160ff30885b1e136
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-28 22:13:24 +0600

    d

---
+++ LeetCode/Premium/2340. Minimum Adjacent Swaps to Make a Valid Array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Premium
+{
+    internal class _2340
+    {
+        public int MinimumSwaps(int[] nums)
+        {
+            var length = nums.Length;
+            if (length == 1)
+                return 0;
+            var min = 0;
+            var max = length - 1;
+
+            for (int i = 1; i < length; i++)
+            {
+                if (nums[i] < nums[min])
+                    min = i;
+            }
+            for (int i = length - 2; i >= 0; i--)
+            {
+                if (nums[i] > nums[max])
+                    max = i;
+            }
+            var total = 0;
+            total += length - max - 1;
+            total += min;
+            if (min >= max)
+                total--;
+            return total;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -84,53 +84,15 @@ var resW2 = new string[]
 };
 
 
-var res1 = new int[] { 2, 2, 2, 2, 2 };
+var res1 = new int[] { 5,3 };
 var res2 = new int[] { 4, 2, 8, 1, 3 };
 
 
-//MedianFinder s = new MedianFinder();
-//s.AddNum(41);
-//s.AddNum(35);
-//Console.WriteLine(s.MaximumSwap(1993));
-
-MedianFinder s = new MedianFinder();
-s.AddNum(41);
-s.AddNum(35);
-
-s.AddNum(62);
-s.AddNum(4);
-
-
-s.AddNum(97);
-s.AddNum(108);
-public class MedianFinder
+Solution s = new Solution();
+Console.WriteLine(s.MinimumSwaps(res1));
+public class Solution
 {
-    PriorityQueue<int, int> low;
-    PriorityQueue<int, int> high;
-    public MedianFinder()
-    {
-        low = new PriorityQueue<int, int>();
-        high = new PriorityQueue<int, int>();
-    }
-    public void AddNum(int num)
-    {
-        low.Enqueue(num, num);
-        var lowVal = low.Dequeue();
-        high.Enqueue(lowVal, -lowVal);
-        if (low.Count < high.Count)
-        {
-            var highVal = high.Dequeue();
-            low.Enqueue(highVal, highVal);
-        }
-    }
-
-    public double FindMedian()
-    {
-        if (low.Count > high.Count)
-            return low.Peek();
-        else
-            return (low.Peek() + high.Peek()) / 2.0;
-    }
+   
 }
 
 public static class Helper
