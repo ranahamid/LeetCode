247ebbd583c989c14b40e5f7a67ba82af89428fe
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-29 03:00:36 +0600

    s

---
+++ LeetCode/2102. Sequentially Ordinal Rank Tracker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SORTracker
+    {
+        /// <summary>
+        /// Runtime 2538 ms Beats 33.33% Memory 90.2 MB Beats 44.44%
+        /// </summary>
+        int counter;
+        SortedList<string, string> sortedList;
+        public SORTracker()
+        {
+            counter = 0;
+            sortedList = new SortedList<string, string>();
+        }
+
+        public void Add(string name, int score)
+        {
+            var key = Int32.MaxValue - score;
+            var keyVal = key + name;
+            sortedList.Add(keyVal, name);
+        }
+
+        public string Get()
+        {
+            var value = sortedList.Values[counter];
+            counter++;
+            return value;
+        }
+    }
+}
