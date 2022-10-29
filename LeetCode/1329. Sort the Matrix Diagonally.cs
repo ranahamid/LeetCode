955821c4a8ea13ffd62063e13072edacf5d88e07
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-30 02:05:45 +0600

    s

--- LeetCode/1329. Sort the Matrix Diagonally.cs
+++ LeetCode/1329. Sort the Matrix Diagonally.cs
@@ -16,7 +16,7 @@ namespace LeetCode
         /// <returns></returns>
         public int[][] DiagonalSort(int[][] mat)
         {
-
+            // sort by /
             var dic = new Dictionary<int, PriorityQueue<int, int>>();
             for (int row = 0; row < mat.Length; row++)
             {
@@ -38,5 +38,35 @@ namespace LeetCode
             }
             return mat;
         }
+        /// <summary>
+        /// Codesignal
+        /// sort matrix diagonal  , matrix sort
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        int[][] solution(int[][] a)
+        {
+            // sort by \
+            var dic = new Dictionary<int, PriorityQueue<int, int>>();
+
+            for (int row = 0; row < a.Length; row++)
+                for (int column = 0; column < a[0].Length; column++)
+                {
+                    if (!dic.ContainsKey(row + column))
+                        dic[row + column] = new PriorityQueue<int, int>();
+                    dic[row + column].Enqueue(a[row][column], -a[row][column]);
+                }
+
+
+            for (int row = 0; row < a.Length; row++)
+            {
+                for (int column = 0; column < a[0].Length; column++)
+                {
+                    a[row][column] = dic[row + column].Dequeue();
+                }
+            }
+            return a;
+        }
+
     }
 }
