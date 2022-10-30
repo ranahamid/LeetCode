43dd32c74a8ace7c4ce639a8fb001ec89ce06c5b
Rana Hamid <rhamid@mymedicalhub.com>
2022-10-30 11:03:10 +0600

    d

---
+++ LeetCode/2451. Odd String Difference.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _2451
+    {
+        public string OddString(string[] words)
+        {
+
+            var dic = new Dictionary<string, List<string>>();
+            foreach (var word in words)
+            {
+                var list = "";
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    int n = word[i + 1] - word[i];
+                    Console.WriteLine(n);
+                    list = list + n.ToString() + "*-*-";
+                }
+
+                dic.TryAdd(list, new List<string>());
+                dic[list].Add(word);
+            }
+            Console.WriteLine(dic.Count);
+
+            foreach (var item in dic)
+            {
+                if (item.Value.Count == 1)
+                {
+                    return item.Value.FirstOrDefault();
+                }
+            }
+            return "    ";
+
+        }
+    }
+}
---
+++ LeetCode/2455. Average Value of Even Numbers That Are Divisible by Three.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _2455
+    {
+        public int AverageValue(int[] nums)
+        {
+            long sum = 0;
+            int counter = 0;
+            foreach (var item in nums)
+            {
+                if (item % 2 == 0 && item % 3 == 0)
+                {
+                    sum += item;
+                    counter++;
+                }
+            }
+            if (counter == 0)
+                return 0;
+            return (int)sum / counter;
+        }
+    }
+}
