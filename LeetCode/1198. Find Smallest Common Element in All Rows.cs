008e64fe75e406f3026b149e44f60b75c66b6aa8
Rana Hamid <ranahamid007@gmail.com>
2022-11-07 18:44:50 +0600

    d

---
+++ LeetCode/1198. Find Smallest Common Element in All Rows.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1198
+    {
+        /// <summary>
+        /// Runtime 3777 ms Beats 7.45% Memory 57 MB Beats 5.32%
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public int SmallestCommonElement(int[][] mat)
+        {
+            var result = mat[0].AsEnumerable();
+            foreach (var item in mat)
+            {
+                var temp = item.AsEnumerable();
+                result = result.Intersect(temp);
+                if (result.Count() == 0)
+                    return -1;
+            }
+            return result.Min();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -87,7 +87,10 @@ var resW2 = new string[]
 var res1 = new int[] { 0, 1, 2, 3 };
 var res2 = new int[] { 4, 2, 8, 1, 3 };
 
-
+public class Solution
+{
+   
+}
 
 public static class Helper
 {
