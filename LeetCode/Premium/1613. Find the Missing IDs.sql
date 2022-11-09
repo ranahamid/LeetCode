7396ca3ef1bd2c173ef7caba80d92a678fde0097
Rana Hamid <ranahamid007@gmail.com>
2022-11-09 19:41:44 +0600

    d

---
+++ LeetCode/Premium/1613. Find the Missing IDs.sql
@@ -0,0 +1,36 @@
+﻿--Runtime 392 ms Beats 89.10%
+-- MY SQL 
+
+--with recursive nums as (
+
+--    select 1 as value 
+--    UNION ALL 
+--    select value+1 as value
+--    from nums
+--    where nums.value<(select max(customer_id) from customers)
+--)
+
+--select value as ids 
+--from nums 
+--where value   not in (select customer_id from customers)
+--order by value
+
+
+-- MS SQL Server
+-- Runtime 691 ms Beats 67.3%
+
+with  nums as (
+
+    select 1 as value 
+    UNION ALL 
+    select value+1 as value
+    from nums
+    where nums.value<=100
+) 
+
+select value as ids 
+from nums 
+where  value< (select max(customer_id) from customers)
+AND 
+value not in (select customer_id from customers)
+order by value
