17181c963208d6953f606fd6b85c3c77c5fb5c55
Rana Hamid <ranahamid007@gmail.com>
2022-11-15 15:47:03 +0600

    _222

---
+++ LeetCode/Z/222. Count Complete Tree Nodes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _222
+    {
+
+        /// <summary>
+        /// Runtime 133 ms Beats 75.35% Memory 41.7 MB Beats 54.23%
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            return 1 + CountNodes(root.left) + CountNodes(root.right);
+        }
+    }
+}
