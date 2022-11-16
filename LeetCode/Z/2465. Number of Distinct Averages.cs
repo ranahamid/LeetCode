1a50225552c86347918d9af953ef3b1404e4cedb
Rana Hamid <ranahamid007@gmail.com>
2022-11-16 19:11:57 +0600

    s

---
+++ LeetCode/Z/2465. Number of Distinct Averages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2465
+    {
+        /// <summary>
+        /// Runtime 105 ms Beats 81.42% Memory 36.6 MB Beats 90.27%
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int DistinctAverages(int[] nums)
+        {
+            Array.Sort(nums);
+            var set = new HashSet<double>();
+            var n = nums.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                set.Add((double)nums[i] + nums[n - i - 1]);
+            }
+            return set.Count;
+        }
+    }
+}
---
+++ LeetCode/Z/2469. Convert the Temperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2469
+    {
+        /// <summary>
+        /// Runtime 154 ms Beats 55.87% Memory 34.6 MB Beats 91.6%
+        /// </summary>
+        /// <param name="celsius"></param>
+        /// <returns></returns>
+        public double[] ConvertTemperature(double celsius)
+        { 
+            var result = new double[2];
+            result[0] = celsius + 273.15;
+            result[1] = celsius * 1.8 + 32;
+            return result;
+        }
+    }
+}
