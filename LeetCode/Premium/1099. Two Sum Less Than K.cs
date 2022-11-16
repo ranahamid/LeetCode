a9f415431f1235108dd8eb9395157e26889410a7
Rana Hamid <rhamid@mymedicalhub.com>
2022-11-16 08:35:35 +0600

    d

--- LeetCode/Premium/1099. Two Sum Less Than K.cs
+++ LeetCode/Premium/1099. Two Sum Less Than K.cs
@@ -8,7 +8,35 @@ namespace LeetCode.Premium
 {
     internal class _1099
     {
+        /// <summary>
+        /// Two pointer
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
         public int TwoSumLessThanK(int[] nums, int k)
+        {
+            int answer = -1;
+            int left = 0;
+            int right = nums.Length - 1;
+            Array.Sort(nums);
+            while (left < right)
+            {
+                var sum = nums[left] + nums[right];
+                if (sum < k)
+                {
+                    answer = Math.Max(answer, sum);
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return answer;
+        }
+        public int TwoSumLessThanK_brute(int[] nums, int k)
         {
             var max = -1;
             for (int i = 0; i < nums.Length; i++)
