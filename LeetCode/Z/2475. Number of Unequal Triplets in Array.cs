526b33efacacba7628e09fa62b625a45a77b3e06
Rana Hamid <ranahamid007@gmail.com>
2022-11-21 17:12:20 +0600

    d

---
+++ LeetCode/Z/2475. Number of Unequal Triplets in Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2475
+    {
+        /// <summary>
+        /// Runtime 160 ms Beats 100% Sorry, there are not enough accepted submissions to show data Memory 36.8 MB Beats 100%
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int UnequalTriplets(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                    for (int k = j + 1; k < nums.Length; k++)
+                        if (nums[i] != nums[j] && nums[j] != nums[k] && nums[i] != nums[k])
+                            counter++;
+            return counter;
+        }
+    }
+}
