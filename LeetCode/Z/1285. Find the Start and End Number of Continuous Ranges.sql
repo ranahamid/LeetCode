99275ec007643a5de674edc6089f36c1be3e5f9a
Rana Hamid <ranahamid007@gmail.com>
2022-11-25 20:22:57 +0600

    row_number

---
+++ LeetCode/Z/1285. Find the Start and End Number of Continuous Ranges.sql
@@ -0,0 +1,10 @@
+﻿ with cte as ( select log_id,
+        row_number() over (order by log_id) as row_number
+        from logs) 
+
+select min(log_id) as         start_id   
+, max(log_id) as end_id       
+from cte
+group by log_id-row_number 
+
+--Runtime 484 ms Beats 71.43%
