ae3246cac34e1b3d6b33153c19ecae9f93e2016e
Rana Hamid <ranahamid007@gmail.com>
2022-11-30 19:54:42 +0600

    s

---
+++ LeetCode/SQL/2480. Form a Chemical Bond.sql
@@ -0,0 +1,10 @@
+﻿select e1.symbol as metal , e2.symbol as nonmetal
+from Elements as e1 ,  Elements as e2 
+where e1.type='Metal' AND e2.type='Nonmetal' 
+
+
+
+--select e1.symbol as metal , e2.symbol as nonmetal
+--from Elements as e1 
+--join by Elements as e2 
+--where e1.type='Metal' AND e2.type='Nonmetal' 
---
+++ LeetCode/Z/2481. Minimum Cuts to Divide a Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2481
+    {
+        /// <summary>
+        /// Runtime 25 ms Beats 87.69% Memory 25 MB Beats 86.15%
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int NumberOfCuts(int n)
+        {
+            if (n == 1)
+                return 0;
+            if (n % 2 == 0)
+                return n / 2;
+            return n;
+        }
+    }
+}
