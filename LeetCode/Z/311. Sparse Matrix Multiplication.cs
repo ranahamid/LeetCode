e699e7c9a40d2564cc1557038a0a38f1016ba6b6
Rana Hamid <ranahamid007@gmail.com>
2022-11-30 20:51:48 +0600

    v

--- LeetCode/Z/311. Sparse Matrix Multiplication.cs
+++ LeetCode/Z/311. Sparse Matrix Multiplication.cs
@@ -20,9 +20,9 @@ namespace LeetCode.Z
                 result[i] = new int[qb];
 
             for (int i = 0; i < m; i++)
-                for (int k = 0; k < n; k++)
-                    for (int j = 0; j < qb; j++)
-                        result[i][j] += mat1[i][k] * mat2[k][j];
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < qb; k++)
+                        result[i][k] += mat1[i][j] * mat2[j][k];
             return result;
         }
     }
