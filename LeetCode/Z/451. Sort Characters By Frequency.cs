a468bd1e1088b3c8dcd72ab107d85c80245b5a9c
Rana Hamid <rhamid@mymedicalhub.com>
2022-12-03 10:30:19 +0600

    FrequencySort

--- LeetCode/Z/451. Sort Characters By Frequency.cs
+++ LeetCode/Z/451. Sort Characters By Frequency.cs
@@ -8,13 +8,32 @@ namespace LeetCode.Z
 {
     internal class _451
     {
+        public string FrequencySort(string s)
+        {
+            var dic = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var ch = s[i];
+                dic.TryAdd(ch, 0);
+                dic[ch]++;
+            }
+            var list = dic.OrderByDescending(x => x.Value).ToList();
+            var sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                for (int j = 0; j < item.Value; j++)
+                    sb.Append(item.Key);
+            }
+            return sb.ToString();
+        }
         /// <summary>
         /// Runtime: 128 ms, faster than 61.49% of C# online submissions for Sort Characters By Frequency.
         ///Memory Usage: 39 MB, less than 76.70% of C# online submissions for Sort Characters By Frequency.
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
-        public string FrequencySort(string s)
+        public string FrequencySort_1(string s)
         {
             var set = GetFrequencyString(s);
             var data = set.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToList();
