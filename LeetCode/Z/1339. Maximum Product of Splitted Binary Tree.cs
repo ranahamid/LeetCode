3505fb0ffdb3c649fd166d5d62a11f2cafa4dd64
Rana Hamid <rhamid@mymedicalhub.com>
2022-12-10 11:47:49 +0600

    TotalSum

---
+++ LeetCode/Z/1339. Maximum Product of Splitted Binary Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _1339
+    {
+        List<long> allSum = new List<long>();
+        public int MaxProduct(TreeNode root)
+        {
+
+            long total = TotalSum(root);
+            long best = 0;
+            foreach (var n in allSum)
+            {
+
+                best = Math.Max(best, n * (total - n));
+            }
+            return (int)(best % 1000000007);
+
+        }
+        public long TotalSum(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            long left = TotalSum(root.left);
+            long right = TotalSum(root.right);
+            long total = root.val + left + right;
+            allSum.Add(total);
+            return total;
+        }
+    }
+}
