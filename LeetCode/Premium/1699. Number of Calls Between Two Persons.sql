39208e0598a784d5401b6ba4babf821403f1aabe
Rana Hamid <rhamid@mymedicalhub.com>
2022-12-11 22:26:34 +0600

    d

---
+++ LeetCode/Premium/1699. Number of Calls Between Two Persons.sql
@@ -0,0 +1,12 @@
+﻿with cte as(
+select LEAST(from_id ,to_id ) as from_id ,GREATEST(from_id ,to_id ) as to_id  , duration 
+    from Calls 
+
+)    
+
+select from_id as person1 , to_id as person2  , count( duration) as call_count   , sum( duration) as total_duration 
+from cte  
+group by from_id, to_id
+
+
+--MY SQL
