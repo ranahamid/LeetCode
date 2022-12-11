871601f58b8af90a2bdbfd04e91f8fe57f713ae7
Rana Hamid <rhamid@mymedicalhub.com>
2022-12-11 21:57:27 +0600

    d

---
+++ LeetCode/Z/2265. Count Nodes Equal to Average of Subtree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2265
+    {
+        /// <summary>
+        /// Runtime 118 ms Beats 80% Memory 39.5 MB Beats 31.43%
+        /// </summary>
+        int counter = 0;
+        public int AverageOfSubtree(TreeNode root)
+        {
+            dfs(root);
+            return counter;
+        }
+        public int[] dfs(TreeNode root)
+        {
+            if (root == null)
+                return new int[] { 0, 0 };
+
+            var left = dfs(root.left);
+            var right = dfs(root.right);
+
+            var currentSum = left[0] + right[0] + root.val;
+            var count = left[1] + right[1] + 1;
+            var avg = currentSum / count;
+            if (avg == root.val)
+                counter++;
+            return new int[] { currentSum, count };
+        }
+    }
+}
