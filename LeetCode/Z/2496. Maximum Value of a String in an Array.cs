3ef6b2ce6f61928179f23e5c1b82adb32dc74ec0
RanaHamid <ranahamid007@gmail.com>
2022-12-16 23:18:01 +0600

    d

---
+++ LeetCode/Z/2496. Maximum Value of a String in an Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2496
+    {/// <summary>
+     /// Runtime 91 ms Beats 86.36% Memory 38.4 MB Beats 20%
+     /// </summary>
+     /// <param name="strs"></param>
+     /// <returns></returns>
+        public int MaximumValue(string[] strs)
+        {
+            var max = 0;
+            foreach (var item in strs)
+            {
+
+
+                var current = 0;
+                if (Int32.TryParse(item, out int n))
+                {
+                    current = Math.Max(current, n);
+                }
+                else
+                {
+                    current = item.Length;
+                }
+                max = Math.Max(max, current);
+            }
+            return max;
+        }
+    }
+}
