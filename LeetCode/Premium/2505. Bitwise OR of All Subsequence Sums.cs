e06f194647f143fbf0700942dc5f550e0162c5b2
RanaHamid <ranahamid007@gmail.com>
2022-12-17 12:51:35 +0600

    SubsequenceSumOr

---
+++ LeetCode/Premium/2505. Bitwise OR of All Subsequence Sums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Premium
+{
+    internal class _2505
+    {
+    }
+}
