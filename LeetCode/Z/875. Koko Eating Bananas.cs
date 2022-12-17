3767295e582b163821b64d9177854c27b3d3cdee
RanaHamid <ranahamid007@gmail.com>
2022-12-17 23:49:20 +0600

    e

--- LeetCode/Z/875. Koko Eating Bananas.cs
+++ LeetCode/Z/875. Koko Eating Bananas.cs
@@ -8,7 +8,37 @@ namespace LeetCode.Z
 {
     internal class _875
     {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            int left = 1;
+            int right = piles.Max();
+            int speed = 1;
+            while (left < right)
+            {
+                int middle = left + (right - left) / 2;
 
+                int hour = 0;
+                foreach (var item in piles)
+                {
+
+                    hour += (int)(Math.Ceiling((double)item / (double)middle));
+                    if (hour > h)
+                    {
+                        break;
+                    }
+                }
+
+                if (hour <= h)
+                {
+                    right = middle;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            return right;
+        }
         #region TLE
         public int MinEatingSpeed_TLE(int[] piles, int h)
         {
