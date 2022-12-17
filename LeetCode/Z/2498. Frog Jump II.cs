2077a9a580decb650c76911fc5fd63b5a7bb8602
RanaHamid <ranahamid007@gmail.com>
2022-12-17 23:42:24 +0600

    _875

---
+++ LeetCode/Z/2498. Frog Jump II.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2498
+    {
+        /// <summary>
+        /// Runtime 238 ms Beats 75.56% Memory 53.1 MB Beats 48.89%
+        /// </summary>
+        /// <param name="stones"></param>
+        /// <returns></returns>
+        public int MaxJump(int[] stones)
+        {
+            var res = stones[1] - stones[0];
+            for (int i = 2; i < stones.Length; i++)
+            {
+                var temp = stones[i] - stones[i - 2];
+                res = Math.Max(res, temp);
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetCode/Z/875. Koko Eating Bananas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _875
+    {
+
+        #region TLE
+        public int MinEatingSpeed_TLE(int[] piles, int h)
+        {
+
+            int speed = 1;
+            while (true)
+            {
+                int hour = 0;
+                foreach (var item in piles)
+                {
+
+                    hour += (int)(Math.Ceiling((double)item / (double)speed));
+                    if (hour > h)
+                    {
+                        break;
+                    }
+                }
+
+                if (hour <= h)
+                {
+                    return speed;
+                }
+                else
+                {
+                    speed++;
+                }
+            }
+        }
+        #endregion
+    }
+}
