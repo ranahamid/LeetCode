77fde8f5e5432b9260ec8ba656ba5f87021d7666
Rana Hamid <rhamid@mymedicalhub.com>
2022-12-20 16:58:41 +0600

    SortColors

---
+++ LeetCode/Z/75. Sort Colors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _75
+    {
+        public void SortColors(int[] nums)
+        {
+            int current = 0, p0 = 0;
+            int p2 = nums.Length - 1;
+
+            while (current <= p2)
+            {
+                if (nums[current] == 0)
+                {
+                    (nums[current], nums[p0]) = (nums[p0], nums[current]);
+                    current++;
+                    p0++;
+                }
+                else if (nums[current] == 2)
+                {
+                    (nums[current], nums[p2]) = (nums[p2], nums[current]);
+                    p2--;
+                }
+                else
+                {
+                    current++;
+                }
+            }
+            #region Dummy
+            public void SortColors_Cheat(int[] nums)
+        {
+            Array.Sort(nums);
+        }
+        #endregion
+    }
+}
