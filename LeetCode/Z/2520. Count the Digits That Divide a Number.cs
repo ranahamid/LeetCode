464738f76dd93307e5d6bc84b00d58b676f6a26f
RanaHamid <ranahamid007@gmail.com>
2023-01-02 10:01:06 +0600

    d

---
+++ LeetCode/Z/2520. Count the Digits That Divide a Number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2520
+    {
+        /// <summary>
+        /// Runtime 31 ms Beats 100% Sorry, there are not enough accepted submissions to show data Memory 27 MB Beats 100%
+        /// </summary>
+        public class Solution
+        {
+            public int CountDigits(int num)
+            {
+
+                var counter = 0;
+                var result = GetDigits(num);
+                foreach (var item in result)
+                {
+                    if (num % item == 0)
+                    {
+                        counter++;
+                    }
+                }
+                return counter;
+            }
+            public List<int> GetDigits(int num)
+            {
+                List<int> result = new List<int>();
+                while (num != 0)
+                {
+                    var temp = num % 10;
+                    result.Add(temp);
+                    num /= 10;
+                }
+                return result;
+            }
+        }
+    }
+}
--- LeetCode/Z/520. Detect Capital.cs
+++ LeetCode/Z/520. Detect Capital.cs
@@ -9,12 +9,46 @@ namespace LeetCode.Z
     internal class _520
     {
         /// <summary>
+        /// Runtime 82 ms Beats 79.17% Memory 39.3 MB Beats 14.17%
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.ToLower() == word)
+                return true;
+            if (word.ToUpper() == word)
+                return true;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (i == 0)
+                {
+
+
+                    if (word[i] == char.ToUpper(word[i]))
+                        continue;
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (word[i] == char.ToLower(word[i]))
+                        continue;
+                    else
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        /// <summary>
         /// Runtime: 125 ms, faster than 30.68% of C# online submissions for Detect Capital.
         /// Memory Usage: 37.4 MB, less than 46.59% of C# online submissions for Detect Capital.
         /// </summary>
         /// <param name="word"></param>
         /// <returns></returns>
-        public bool DetectCapitalUse(string word)
+        public bool DetectCapitalUse_1(string word)
         {
             if (word == word.ToLower() || word == word.ToUpper() || word == TitleCase(word))
             {
