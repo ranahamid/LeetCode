3b1f25e3479d4e4d348bede0b59a2d0747977fca
Rana Hamid <rhamid@mymedicalhub.com>
2023-01-02 20:41:41 +0600

    GetPrimeFactors

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -106,15 +106,43 @@ var resW2 = new char[]
 var res1 = new int[] { 91, 4, 64, 78 };
 var res2 = new int[] { 1, 0, 0, -1, 0, 0, -1, 0, 0, 1 };
 
-Solution s = new Solution();
-Console.WriteLine(s.ClosetTarget(resW1, "leetcode", 0));
- 
+//Solution s = new Solution();
+//Console.WriteLine(s.ClosetTarget(resW1, "leetcode", 0));
+
+public class Solution
+{
 
 
+}
 
 
 public static class Helper
 {
+    public static HashSet<int>GetPrimeFactors(int n)
+    {
+        HashSet<int> result= new HashSet<int>();
+        while (n % 2==0)
+        {
+            if(!result.Contains(2))
+                result.Add(2);
+            n /= 2;
+        }
+        for(int i = 3; i <= Math.Sqrt(n); i++)
+        {
+            while (n % i == 0)
+            {
+                if (!result.Contains(i))
+                    result.Add(i);
+                n/= i;
+            }
+        }
+        if(n>2 && !result.Contains(n))
+        {
+            result.Add(n);
+        }
+        return result;
+    }
+
     private static IEnumerable<int> GetDivisors(int number)
     {
         if (number <= 0) { yield return default; }
---
+++ LeetCode/Z/2521. Distinct Prime Factors of Product of Array.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2521
+    {
+        /// <summary>
+        /// Runtime 134 ms Beats 100% Sorry, there are not enough accepted submissions to show data Memory 51.1 MB Beats 100%
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int DistinctPrimeFactors(int[] nums)
+        {
+            HashSet<int> result = new HashSet<int>();
+            foreach (var n in nums)
+            {
+                var primes = GetPrimeFactors(n);
+                result.UnionWith(primes);
+            }
+            return result.Count;
+        }
+        public static HashSet<int> GetPrimeFactors(int n)
+        {
+            HashSet<int> result = new HashSet<int>();
+            while (n % 2 == 0)
+            {
+                if (!result.Contains(2))
+                    result.Add(2);
+                n /= 2;
+            }
+            for (int i = 3; i <= Math.Sqrt(n); i++)
+            {
+                while (n % i == 0)
+                {
+                    if (!result.Contains(i))
+                        result.Add(i);
+                    n /= i;
+                }
+            }
+            if (n > 2 && !result.Contains(n))
+            {
+                result.Add(n);
+            }
+            return result;
+        }
+    }
+}
