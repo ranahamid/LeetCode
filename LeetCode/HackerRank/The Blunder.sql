2e191cdaf392768d608ac55682d6a239512b35c9
Rana Hamid <rhamid@mymedicalhub.com>
2023-02-07 17:25:02 +0600

    d

---
+++ LeetCode/HackerRank/The Blunder.sql
@@ -0,0 +1,12 @@
+﻿select   
+
+
+    CEILING(
+    avg( CAST( salary  AS DECIMAL(10,2)))
+    
+    -
+    avg(CAST(REPLACE(CAST(salary AS varchar), '0', '') AS DECIMAL(10,2)) )
+    
+        )
+ 
+from EMPLOYEES 
