d3c0721f2d0368b6b6955960881c553f86109bce
Rana Hamid <rhamid@mymedicalhub.com>
2023-02-10 19:33:56 +0600

    d

---
+++ LeetCode/Z/1968. Array With Elements Not Equal to Average of Neighbors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _1968
+    {
+        public int[] RearrangeArray(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length - 1; i = i + 2)
+                (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+            return nums;
+        }
+        public int[] RearrangeArray_1(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i = i + 2)
+                (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+            return nums;
+        }
+    }
+}
