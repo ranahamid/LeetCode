fdf64e1630fc3318218a0715e7bb760357010185
RanaHamid <ranahamid007@gmail.com>
2023-02-12 18:57:39 +0600

    d

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -118,38 +118,7 @@ Console.WriteLine(s.CategorizeBox(2909
 
 public class Solution
 {
-    public int[] VowelStrings(string[] words, int[][] queries)
-    {
-        var vowels = new bool[words.Length];
-        var chars = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
-        var prefixSum = new int[words.Length+1];
-        prefixSum[0] = 0;
-        int i = 0;
-        for (i = 0; i < words.Length; i++)
-        {
-            var length = words[i].Length;
-            if (i != 0)
-                prefixSum[i+1] = prefixSum[i];
-
-            if (chars.Contains(words[i][0]) && chars.Contains(words[i][length - 1]))
-            {
-                vowels[i] = true;
-                if (i == 0)
-                    prefixSum[i+1] = 1;
-                else
-                    prefixSum[i+1] = prefixSum[i ] + 1;
-            }
-        }
-        var result = new int[queries.Length];
-        i = 0;
-        foreach (var q in queries)
-        {
-            int val = prefixSum[q[1]+1] - prefixSum[q[0]];
-            result[i++] = val;
-        }
-
-        return result;
-    }
+   
 }
 
 
---
+++ LeetCode/Z/2477. Minimum Fuel Cost to Report to the Capital.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2477
+    {
+        long fuel = 0;
+        public long MinimumFuelCost(int[][] roads, int seats)
+        {
+            var adj = new Dictionary<int, List<int>>();
+            foreach (var item in roads)
+            {
+                adj.TryAdd(item[0], new List<int>());
+                adj[item[0]].Add(item[1]);
+
+                adj.TryAdd(item[1], new List<int>());
+                adj[item[1]].Add(item[0]);
+
+            }
+            dfs(0, -1, adj, seats);
+            return fuel;
+        }
+        public long dfs(int node, int parent, Dictionary<int, List<int>> adj, int seats)
+        {
+            long respresentative = 1;
+            if (!adj.ContainsKey(node))
+            {
+                return respresentative;
+            }
+            foreach (var child in adj[node])
+            {
+                if (child != parent)
+                {
+                    respresentative += dfs(child, node, adj, seats);
+                }
+            }
+            if (node != 0)
+            {
+                fuel += (long)Math.Ceiling((double)respresentative / seats);
+            }
+            return respresentative;
+        }
+    }
+}
