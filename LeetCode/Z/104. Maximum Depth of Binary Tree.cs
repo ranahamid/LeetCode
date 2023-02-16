d519f448c8bcb1da1f30da525a7625471b1c9108
RanaHamid <ranahamid007@gmail.com>
2023-02-16 11:33:34 +0600

    MaxDepth

---
+++ LeetCode/Z/104. Maximum Depth of Binary Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _104
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+            return 1 + Math.Max(left, right);
+        }
+    }
+}
