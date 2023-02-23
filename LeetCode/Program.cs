d44ad09214f1d5f6bef7c7a5c4a0d996e0064f21
Rana Hamid <rhamid@mymedicalhub.com>
2023-02-23 13:37:36 +0600

    ShipWithinDays

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -117,40 +117,7 @@ Console.WriteLine(s.ShipWithinDays(res1, 5));
 
 public class Solution
 {
-    public int ShipWithinDays(int[] weights, int days)
-    {
-        var max = weights.Max();
-        var total = weights.Sum();
-        int left = max, right = total;
-        while (left<right)
-        {
-            var mid=(left+right)/2;
-            if (IsValid(weights, mid, days))
-            {
-                right=mid;
-            }
-            else
-            {
-                left = mid + 1;
-            }
-        } 
-        return left;
-    } 
-    public bool IsValid(int[] weights, int mid, int days)
-    {
-        int dayCounter = 1;
-        var total = 0;
-        foreach (var weight in weights)
-        {
-            total += weight;
-            if (total > mid)
-            {
-                dayCounter++;
-                total = weight;
-            }
-        }
-        return dayCounter <= days;
-    }
+   
 }
 
 
---
+++ LeetCode/Z/1011. Capacity To Ship Packages Within D Days.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _1011
+    {
+        public int ShipWithinDays(int[] weights, int days)
+        {
+            var max = weights.Max();
+            var total = weights.Sum();
+            int left = max, right = total;
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+                if (IsValid(weights, mid, days))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+        public bool IsValid(int[] weights, int mid, int days)
+        {
+            int dayCounter = 1;
+            var total = 0;
+            foreach (var weight in weights)
+            {
+                total += weight;
+                if (total > mid)
+                {
+                    dayCounter++;
+                    total = weight;
+                }
+            }
+            return dayCounter <= days;
+        }
+    }
+}
