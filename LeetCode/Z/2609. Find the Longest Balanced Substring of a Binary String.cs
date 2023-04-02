746c53c202f9289644d524e6552fcdba66ec19bb
RanaHamid <ranahamid007@gmail.com>
2023-04-02 10:35:15 +0600

    1

---
+++ LeetCode/Z/2609. Find the Longest Balanced Substring of a Binary String.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2609
+    {
+        public int FindTheLongestBalancedSubstring(string s)
+        {
+            var max = 0;
+            var start = 0;
+            var zero = 0;
+            var one = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    if (one == 0)
+                        zero++;
+                    else
+                    {
+                        zero = 1;
+                        one = 0;
+                    }
+                }
+                if (s[i] == '1')
+                {
+                    if (zero > one)
+                        one++;
+
+                    max = Math.Max(one * 2, max);
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Z/2610. Convert an Array Into a 2D Array With Conditions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Z
+{
+    internal class _2610
+    {
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            var dic = new SortedDictionary<int, int>();
+            foreach (var item in nums)
+            {
+                dic.TryAdd(item, 0);
+                dic[item]++;
+            }
+            var max = dic.Select(x => x.Value).Max();
+            var dicItems = dic.Select(x => x.Key).ToList();
+
+            for (int i = 0; i < max; i++)
+            {
+                List<int> row = new List<int>();
+                foreach (var item in dicItems)
+                {
+
+                    if (dic[item] > 0)
+                    {
+                        dic[item]--;
+                        row.Add(item);
+                    }
+                }
+                result.Add(row);
+            }
+
+            return result;
+        }
+    }
+}
